1f2ac2e0f8d90e96123156480203dd85590ee85d
JanuaryClouds <janmichaelcristianbcruz@gmail.com>
2025-07-18 15:51:17 +0800

    Created Table for Burial History

---
+++ resources/views/components/burial-service-history-table.blade.php
@@ -0,0 +1,29 @@
+<table>
+    <tr class="*:text-gray-400 *:text-left *:py-2 *:px-4 *:font-semibold border-b-2 border-b-gray-200 border-gray-100 border-2 rounded-t-lg">
+        <th>Name of Deceased</th>
+        <th>Address of Burial</th>
+        <th>Date of Burial</th>
+        <th>Funeral Service</th>
+        <th>Funds Collected</th>
+        <th>Action</th>
+    </tr>
+    <tr class="*:py-2 *:px-4 border-2 border-gray-100">
+        <td>Sample Name</td>
+        <td>Sample Address</td>
+        <td>Sample Date</td>
+        <td>Sample Service</td>
+        <td>Sample Fund</td>
+        <td class="flex items-center gap-2">
+
+            <!-- TODO: Add a modal to show the image -->
+            <button class="bg-yellow-300 rounded-lg p-1">
+                <i class="material-icons text-black">image_search</i>
+            </button>
+
+            <!-- TODO: Button to Redirect to a dedicated page for burial service details -->
+            <button class="bg-gray-400 rounded-lg p-1">
+                <i class="material-icons">open_in_full</i>
+            </button>
+        </td>
+    </tr>
+</table>
