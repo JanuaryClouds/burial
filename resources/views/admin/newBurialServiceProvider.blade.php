de3c1cb579b8295bffca76bb1d6f002e2df3999f
JanuaryClouds <janmichaelcristianbcruz@gmail.com>
2025-07-21 09:55:02 +0800

    Added view for New burial service provider

---
+++ resources/views/admin/newBurialServiceProvider.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.admin');
+@section('content')
+
+<div class="flex flex-col gap-12">
+    <h2 class="font-bold text-center text-xl text-gray-700">
+        New Burial Service Provider
+    </h2>
+
+    <x-burial-service-provider-form />
+</div>
+
+@endsection
