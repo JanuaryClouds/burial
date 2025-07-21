267c5ed5a07d112da72ad8de8d260f5989375c4d
JanuaryClouds <janmichaelcristianbcruz@gmail.com>
2025-07-21 09:53:14 +0800

    Added table to list burial service providers

---
+++ resources/views/components/burial-service-provider-table.blade.php
@@ -0,0 +1,21 @@
+<table>
+    <tr class="*:text-gray-400 *:text-left *:py-2 *:px-4 *:font-semibold border-b-2 border-b-gray-200 border-gray-100 border-2 rounded-t-lg">
+        <th>Name of Provider</th>
+        <th>Address</th>
+        <th>Contact</th>
+        <th>No. of Services</th>
+        <th>Actions</th>
+    </tr>
+    <tr class="*:py-2 *:px-4 border-2 border-gray-100">
+        <td>Sample Name</td>
+        <td>Sample Address</td>
+        <td>Sample Contact</td>
+        <td>Sample Fund</td>
+        <td class="flex items-center gap-2">
+            <!-- TODO: Add a modal to show the image -->
+            <button class="bg-yellow-300 rounded-lg p-2">
+                <i class="fa-solid fa-pen"></i>
+            </button>
+        </td>
+    </tr>
+</table>
