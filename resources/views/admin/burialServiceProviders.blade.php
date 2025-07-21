4263a347179f4726446a03e78787b9a9b291acf4
JanuaryClouds <janmichaelcristianbcruz@gmail.com>
2025-07-21 09:53:38 +0800

    Added view for burial service providers

---
+++ resources/views/admin/burialServiceProviders.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.admin');
+@section('content')
+
+<div class="flex flex-col gap-12">
+    <header class="flex justify-between items-center">
+        <h2 class="font-bold text-xl text-gray-700">Burial Service Providers</h2>
+
+        <a href="{{ route('admin.burial.new.provider') }}" class="px-4 py-2 text-white font-semibold tracking-widest uppercase text-sm bg-gray-700 rounded-lg hover:bg-gray-300 hover:text-black transition-colors">
+            New Burial Service Provider
+        </a>
+    </header>
+    <x-burial-service-provider-table />
+</div>
+
+@endsection
