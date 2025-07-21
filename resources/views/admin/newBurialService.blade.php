67e5491da3b8c528da0620bcb5155c39914f5d8e
JanuaryClouds <janmichaelcristianbcruz@gmail.com>
2025-07-21 09:54:20 +0800

    Removed form tag

--- resources/views/admin/newBurialService.blade.php
+++ resources/views/admin/newBurialService.blade.php
@@ -2,9 +2,7 @@
 @section('content')
 
 <div class="flex flex-col gap-12">
-    <form action="" method="post">
-        <h2 class="font-bold text-center text-xl text-gray-700">New Burial Service</h2>
-    </form>
+    <h2 class="font-bold text-center text-xl text-gray-700">New Burial Service</h2>
 
     <x-burial-service-form />
 </div>
