06cac53d598a3cefaf1b588a651b896c9e7d4534
JanuaryClouds <janmichaelcristianbcruz@gmail.com>
2025-07-28 14:31:24 +0800

    Added business logic for Providers

---
+++ app/Http/Requests/BurialServiceRequest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class BurialServiceRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            "deceased_firstname" => "string|required|max:255",
+            "deceased_lastname" => "string|required|max:255",
+            "representative" => "string|required|max:255",
+            "representative_contact" => "string|required|max:255",
+            "rep_relationship" => "required|numeric|exists:relationships,id",
+            "burial_address" => "string|required|max:255",
+            "barangay_id" => "required|numeric|exists:barangays,id",
+            "start_of_burial" => "date|required",
+            "end_of_burial"=> "date|required",
+            "burial_service_provider" => "required|numeric|exists:burial_service_providers,id",
+            "collected_funds" => "string|required|max:255",
+            "remarks"=> "nullable|string|max:255",
+        ];
+    }
+}
