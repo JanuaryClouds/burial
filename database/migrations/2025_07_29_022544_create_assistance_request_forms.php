d02a98e6e92de3db5709ba911ab81430f2fddf13
JanuaryClouds <janmichaelcristianbcruz@gmail.com>
2025-07-30 15:42:30 +0800

    Added: Table for burial assistance request

---
+++ database/migrations/2025_07_29_022544_create_assistance_request_forms.php
@@ -0,0 +1,53 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('burial_assistance_requests', function (Blueprint $table) {
+            $table->uuid()->primary();
+            $table->string('deceased_firstname');
+            $table->string('deceased_lastname');
+            $table->string('representative');
+            $table->string('representative_contact');
+            $table->foreignId('rep_relationship')
+                ->constrained('relationships')
+                ->noActionOnDelete()
+                ->noActionOnUpdate();
+            $table->string('burial_address');
+            $table->foreignId('barangay_id')
+                ->constrained('barangays')
+                ->noActionOnDelete()
+                ->noActionOnUpdate();
+            $table->datetime('start_of_burial');
+            $table->datetime('end_of_burial');
+            $table->enum("status", [
+                'pending',
+                'approved',
+                'rejected',
+            ])->default('pending');
+            $table->foreignId('type_of_assistance')
+                ->default(8)
+                ->constrained('assistances')
+                ->cascadeOnDelete()
+                ->cascadeOnUpdate();
+            $table->string('remarks')->nullable()->max(255);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('assistance_request_forms');
+    }
+};
