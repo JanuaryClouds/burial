53ff3ac81196915bd6036daf62b2808a400db8d8
JanuaryClouds <janmichaelcristianbcruz@gmail.com>
2025-07-31 15:07:35 +0800

    added: function to check if admin

--- app/Models/User.php
+++ app/Models/User.php
@@ -31,4 +31,9 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+
+    public function isAdmin ()
+    {
+        return $this->hasRole('admin');
+    }
 }
