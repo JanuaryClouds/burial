667afdce27fd3ba394b9597be89a91fe51fdad19
JanuaryClouds <janmichaelcristianbcruz@gmail.com>
2025-07-31 15:05:32 +0800

    added: summary data for dashboard

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,7 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\BurialServiceRequest;
 use Illuminate\Http\Request;
+use App\Models\burialAssistanceRequest;
+use App\Models\BurialServiceProvider;
+use App\Models\BurialService;
+use Illuminate\Support\Facades\DB;
 
 class DashboardController extends Controller
 {
@@ -13,7 +18,45 @@ class DashboardController extends Controller
 
     public function admin()
     {
-        return view('admin.dashboard');
+        $requestsData = burialAssistanceRequest::select('barangay_id', DB::raw('count(*) as total'))
+            ->with('barangay')
+            ->groupBy('barangay_id')
+            ->where('status','pending')
+            ->get()
+            ->map(function ($item) {
+                return [
+                    'name' => $item->barangay->name ?? 'Unknown',
+                    'count' => $item->total,
+                ];
+            });
+
+        $providersData = BurialServiceProvider::select('barangay_id', DB::raw('count(*) as total'))
+            ->with('barangay')
+            ->groupBy('barangay_id')
+            ->get()
+            ->map(function ($item) {
+                return [
+                    'name' => $item->barangay->name ?? 'Unknown',
+                    'count' => $item->total,
+                ];
+            });
+
+        $servicesData = BurialService::select('barangay_id',
+        DB::raw('count(*) as total'))
+            ->with('barangay')
+            ->groupBy('barangay_id')
+            ->get()
+            ->map(function ($item) {
+                return [
+                    'name' => $item->barangay->name ?? 'Unknown',
+                    'count' => $item->total,
+                ];
+            });
+        
+        $serviceRequests = burialAssistanceRequest::getBurialAssistanceRequests('pending');
+        $providers = BurialServiceProvider::all();
+        $services = BurialService::all();
+        return view('admin.dashboard', compact('serviceRequests', 'providers', 'services', 'requestsData', 'providersData', 'servicesData'));
     }
 
     public function user()
