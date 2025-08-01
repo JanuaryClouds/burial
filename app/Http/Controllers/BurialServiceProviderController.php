e2a09e46a28ab4cb4c7213d572fd174f29e9d897
JanuaryClouds <janmichaelcristianbcruz@gmail.com>
2025-08-01 15:12:23 +0800

    added: functions for viewing and updating

--- app/Http/Controllers/BurialServiceProviderController.php
+++ app/Http/Controllers/BurialServiceProviderController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\BurialServiceProvider;
 use Illuminate\Http\Request;
+use App\Models\Barangay;
 use App\Services\BurialServiceProviderService;
 use App\Http\Requests\BurialServiceProviderRequest;
 
@@ -18,6 +20,15 @@ class BurialServiceProviderController extends Controller
         return view('admin.newBurialServiceProvider');
     }
 
+    public function view($id) {
+        $serviceProvider = BurialServiceProvider::where('id', $id)->first();
+        $barangays = Barangay::getAllBarangays();
+        if (!$serviceProvider) {
+            return redirect()->route('admin.burial.providers')->with('error','Burial service provider not found.');
+        }
+        return view('admin.update-provider', compact('serviceProvider', 'barangays'));
+    }
+
     public function store(BurialServiceProviderRequest $request) {
         $data = $request->validated();
         $provider = $this->BurialServiceProviderService->store($data);
@@ -27,5 +38,15 @@ class BurialServiceProviderController extends Controller
         }
 
         return redirect()->back()->withErrors(['error' => 'Failed to create burial service provider.']);
+    }
+
+    public function update(BurialServiceProviderRequest $request, $id) {
+        $data = $request->validated();
+        $provider = BurialServiceProvider::find($id);
+        if (!$provider) {
+            return redirect()->route('admin.burial.providers')->with('error','Burial service provider not found.');
+        }
+        $provider->update($data);
+        return redirect()->route('admin.burial.providers')->with('success','Successfully updated the provider\'s information.');
     }
 }
