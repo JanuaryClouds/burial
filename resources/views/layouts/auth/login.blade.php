23c7a57b1610b8acc2868e8453ec83813ed489f4
JanuaryClouds <janmichaelcristianbcruz@gmail.com>
2025-08-05 15:20:33 +0800

    updated: use Bootstrap theme

--- resources/views/layouts/auth/login.blade.php
+++ resources/views/layouts/auth/login.blade.php
@@ -1,17 +1,21 @@
 <!DOCTYPE html>
 <html lang="en" x-data>
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <script src="https://kit.fontawesome.com/4f2d7302b1.js" crossorigin="anonymous"></script>
-    <title>Laravel tailwin template</title>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="https://cdn.datatables.net/1.13.6/js/jquery.dataTables.min.js"></script>
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-LN+7fdVzj6u52u30Kp6M/trliBMCMKTyK833zpbD+pXdCLuTusPj697FH4R/5mcr" crossorigin="anonymous">
+    <title>Login - CSWDO Burial Assistance</title>
     @vite('resources/css/app.css')
     @vite('resources/js/app.js')
 </head>
 
-<body class="bg-gray-100">
+<body class="min-vh-100" style="background: url('{{ asset('images/cover.webp') }}') no-repeat center center / cover;">
     @yield('content')
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/js/bootstrap.bundle.min.js" integrity="sha384-ndDqU0Gzau9qJ1lfW4pNLlhNTkCfHzAVBReH9diLvGRem5+R9g2FzA8ZGN954O5Q" crossorigin="anonymous"></script>
 </body>
 
 </html>
