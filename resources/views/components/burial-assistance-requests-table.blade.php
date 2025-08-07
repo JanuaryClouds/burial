5793b1f7664efeb5afd4d66785139819921fc643
JanuaryClouds <janmichaelcristianbcruz@gmail.com>
2025-08-07 15:23:38 +0800

    updated: use bootstrap and datatable

--- resources/views/components/burial-assistance-requests-table.blade.php
+++ resources/views/components/burial-assistance-requests-table.blade.php
@@ -1,80 +1,39 @@
-<div class="flex flex-col gap-2" x-data="{ showRequest: 'pending' }">
-    <span class="flex justify-content-start align-items-center *:py-2 *:px-3 *:hover:cursor-pointer gap-2 *:rounded-md">
-        <button @click="showRequest = 'pending'"
-            x-bind:class="showRequest === 'pending' ? 'bg-red-400 text-white' : 'bg-gray-200'"
-            x-transition
-        >
-            Pending
-        </button>
-        <button @click="showRequest = 'approved'"
-            x-bind:class="showRequest === 'approved' ? 'bg-red-400 text-white' : 'bg-gray-200'"
-            x-transition
-            x-transition
-        >
-            Approved
-        </button>
-        <button @click="showRequest = 'rejected'"
-            x-bind:class="showRequest === 'rejected' ? 'bg-red-400 text-white' : 'bg-gray-200'"
-            x-transition
-        >
-            Rejected
-        </button>
-    </span>
-
-    <table>
-        <thead class="border-2 border-gray-200 gap-4">
-            <tr class="*:text-gray-400 *:text-left *:py-2 *:px-4 *:font-semibold border-b-2 border-b-gray-200 border-gray-100 border-2 rounded-t-lg">
-                <td>Name of Deceased</td>
-                <td>Representative / Contact Details</td>
-                <td>Address</td>
-                <td>Duration of Burial</td>
-                <td></td>
+<table id="requestsTable" class="table table-borderless">
+    <thead>
+        <tr class="bg-primary">
+            <th class="text-white">Name of Deceased</th>
+            <th class="text-white">Representative / Contact Details</th>
+            <th class="text-white">Address</th>
+            <th class="text-white">Duration of Burial</th>
+            <th class="text-white">Action</th>
+        </tr>
+    </thead>
+    <tbody>
+        @foreach ($allRequests as $request)
+            <tr>
+                <td>{{ $request->deceased_firstname }} {{ $request->deceased_lastname }}</td>
+                <td>{{ $request->representative }} / {{ $request->representative_contact }}</td>
+                <td>{{ $request->burial_address }}, {{ $request->barangay->name }}</td>
+                <td>{{ Str::limit($request->start_of_burial, 10) }}, {{ Str::limit($request->end_of_burial, 10) }}</td>
+                <td>
+                    <a href="{{ route('admin.burial.request.view', ['uuid' => $request->uuid]) }}" class="btn btn-primary">
+                        <i class="fa-solid fa-arrow-up-right-from-square"></i>
+                    </a>
+                </td>
             </tr>
-        </thead>
-        <tbody x-show="showRequest === 'pending'" x-cloak x-transition">
-            @foreach ($pendingRequests as $request)
-                <tr class="border-2 border-gray-100 *:py-2 *:px-4" x-cloak x-transition>
-                    <td>{{ $request->deceased_firstname }} {{ $request->deceased_lastname }}</td>
-                    <td>{{ $request->representative }} / {{ $request->representative_contact }}</td>
-                    <td>{{ $request->burial_address }}, {{ $request->barangay->name }}</td>
-                    <td>{{ Str::limit($request->start_of_burial, 10) }}, {{ Str::limit($request->end_of_burial, 10) }}</td>
-                    <td>
-                        <a href="{{ route('admin.burial.request.view', ['uuid' => $request->uuid]) }}">
-                            <i class="fa-solid fa-arrow-up-right-from-square"></i>
-                        </a>
-                    </td>
-                </tr>
-            @endforeach
-        </tbody>
-        <tbody x-show="showRequest === 'approved'" x-cloak x-transition">
-            @foreach ($approvedRequests as $request)
-                <tr class="border-2 border-gray-100 *:py-2 *:px-4" x-cloak x-transition>
-                    <td>{{ $request->deceased_firstname }} {{ $request->deceased_lastname }}</td>
-                    <td>{{ $request->representative }} / {{ $request->representative_contact }}</td>
-                    <td>{{ $request->burial_address }}, {{ $request->barangay->name }}</td>
-                    <td>{{ Str::limit($request->start_of_burial, 10) }}, {{ Str::limit($request->end_of_burial, 10) }}</td>
-                    <td>
-                        <a href="{{ route('admin.burial.request.view', ['uuid' => $request->uuid]) }}">
-                            <i class="fa-solid fa-arrow-up-right-from-square"></i>
-                        </a>
-                    </td>
-                </tr>
-            @endforeach
-        </tbody>
-        <tbody x-show="showRequest === 'rejected'" x-cloak x-transition">
-            @foreach ($rejectedRequests as $request)
-                <tr class="border-2 border-gray-100 *:py-2 *:px-4" x-cloak x-transition>
-                    <td>{{ $request->deceased_firstname }} {{ $request->deceased_lastname }}</td>
-                    <td>{{ $request->representative }} / {{ $request->representative_contact }}</td>
-                    <td>{{ $request->burial_address }}, {{ $request->barangay->name }}</td>
-                    <td>{{ Str::limit($request->start_of_burial, 10) }}, {{ Str::limit($request->end_of_burial, 10) }}</td>
-                    <td>
-                        <a href="{{ route('admin.burial.request.view', ['uuid' => $request->uuid]) }}">
-                            <i class="fa-solid fa-arrow-up-right-from-square"></i>
-                        </a>
-                    </td>
-                </tr>
-            @endforeach
-        </tbody>
-    </table>
-</div>
+        @endforeach
+    </tbody>
+</table>
+
+<script>
+    document.addEventListener('DOMContentLoaded', () => {
+        console.log('DOM fully loaded and parsed');
+        const table = document.querySelector('#requestsTable');
+        if (table) {
+            $(table).DataTable({
+                responsive: true,
+                pageLength: 10,
+            });
+        }
+    });
+</script>
--- resources/views/components/burial-service-provider-table.blade.php
+++ resources/views/components/burial-service-provider-table.blade.php
@@ -1,21 +1,38 @@
-<table>
-    <tr class="*:text-gray-400 *:text-left *:py-2 *:px-4 *:font-semibold border-b-2 border-b-gray-200 border-gray-100 border-2 rounded-t-lg">
-        <th>Name of Provider</th>
-        <th>Address</th>
-        <th>Contact</th>
-        <th>Actions</th>
-    </tr>
-    @foreach ($providers as $provider)
-    <tr class="*:py-2 *:px-4 border-2 border-gray-100">
-        <td>{{ $provider->name }}</td>
-        <td>{{ $provider->address }}, {{ $provider->barangay->name }}</td>
-        <td>{{ $provider->contact_details }}</td>
-        <td class="flex items-center gap-2">
-            <!-- TODO: Add a modal to show the image -->
-            <a href="{{ route('admin.burial.provider.view', ['id' => $provider->id]) }}" class="bg-yellow-300 rounded-lg p-2 cursor:pointer">
-                <i class="fa-solid fa-pen"></i>
-            </a>
-        </td>
-    </tr>
-    @endforeach
+<table id="providersTable" class="table table-borderless">
+    <thead>
+        <tr class="bg-primary">
+            <th class="text-white">Name of Provider</th>
+            <th class="text-white">Address</th>
+            <th class="text-white">Contact</th>
+            <th class="text-white">Actions</th>
+        </tr>
+    </thead>
+    <tbody>
+        @foreach ($providers as $provider)
+        <tr class="">
+            <td>{{ $provider->name }}</td>
+            <td>{{ $provider->address }}, {{ $provider->barangay->name }}</td>
+            <td>{{ $provider->contact_details }}</td>
+            <td class="flex items-center gap-2">
+                <!-- TODO: Add a modal to show the image -->
+                <a href="{{ route('admin.burial.provider.view', ['id' => $provider->id]) }}" class="btn btn-primary">
+                    <i class="fa-solid fa-pen"></i>
+                </a>
+            </td>
+        </tr>
+        @endforeach
+    </tbody>
 </table>
+
+<script>
+    document.addEventListener('DOMContentLoaded', () => {
+        console.log('DOM fully loaded and parsed');
+        const table = document.querySelector('#providersTable');
+        if (table) {
+            $(table).DataTable({
+                responsive: true,
+                pageLength: 10,
+            });
+        }
+    });
+</script>
