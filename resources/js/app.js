0b755d4241ba5df9c82ac40981517de88cd1660d
JanuaryClouds <janmichaelcristianbcruz@gmail.com>
2025-08-07 15:21:25 +0800

    added: jquery and datatables for tables views

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,13 @@
 import "./bootstrap";
 import './stisla';
+import $ from 'jquery';
 import Alpine from "alpinejs";
+import 'datatables.net';
+import 'datatables.net-responsive';
+import 'datatables.net-bs5';
+import 'datatables.net-responsive-bs5'
 
 window.Alpine = Alpine;
+window.$ = window.jQuery = $
 
 Alpine.start();
