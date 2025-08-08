2c681874c0c0624a965940a2e97a67619496a035
JanuaryClouds <janmichaelcristianbcruz@gmail.com>
2025-08-08 15:10:56 +0800

    updated: applied bootstrap classes to updating provider

--- resources/views/admin/update-provider.blade.php
+++ resources/views/admin/update-provider.blade.php
@@ -10,66 +10,66 @@
 @if (session('error'))
     <x-alert type="error" :message="session('error')" />
 @endif
-<div>
-    <form action="{{ route('admin.burial.provider.update', ['id' => $serviceProvider->id]) }}" method="POST" id="burialServiceProviderForm" class="flex flex-col items-center gap-12">
+<div class="container d-flex flex-column justify-content-between align-items-center">
+    <form action="{{ route('admin.burial.provider.update', ['id' => $serviceProvider->id]) }}" method="POST" id="burialServiceProviderForm" class="row flex-column align-items-center shadow w-50 p-2 gap-4">
     @csrf
     @method('PUT')
 
-    <div class="flex flex-col gap-4 bg-white shadow-lg rounded-md p-4 mx-36">
-        <header class="flex justify-between">
-            <span class="flex justify-between items-center gap-2">
-                <img src="{{ asset('images/CSWDO.webp') }}" alt="" class="w-24 mr-2">
-                <span class="flex flex-col gap-2">
-                    <h3 class="text-4xl font-semibold">Burial Service Provider Form</h3>
+    <div class="row flex-column w-100 gap-4">
+        <header class="row d-flex">
+            <span class="d-flex gap-2">
+                <img src="{{ asset('images/CSWDO.webp') }}" alt="" class="mr-2" style="width: 100px;">
+                <span class="d-flex flex-column justify-content-center">
+                    <h3 class="fw-semibold">Burial Service Provider Form</h3>
                     <p class="text-lg text-gray-600">CSWDO Burial Assistance</p>
                 </span>
             </span>
         </header>
-        <p class="text-gray-400 text-xs">All fields marked by (*) are required</p>
-        <hr class="border-2 border-gray-700">
-        <div class="flex flex-col gap-4">
-            <h4 class="text-lg font-semibold">Details of the Provider</h4>
-            <div class="flex justify-between items-start w-full gap-2">
-                <span class="flex flex-col w-2/3 justify-between">
-                    <input type="text" required value="{{ $serviceProvider->name }}" name="name" id="name" class="border-2 border-gray-300 rounded-md px-2 py-1">
-                    <label for="name" class="text-sm text-gray-400 text-center">Name of Company*</label>
+        <small class="">All fields marked by (*) are required</small>
+        <hr class="border-1">
+        <div class="d-flex flex-column gap-1">
+            <h4 class="text-black tw-semibold">Details of the Provider</h4>
+            <div class="d-flex justify-content-between align-items-start w-100 gap-1">
+                <span class="d-flex flex-column w-75 justify-content-between">
+                    <input type="text" required value="{{ $serviceProvider->name }}" name="name" id="name" class="form-control">
+                    <label for="name" class="form-label">Name of Company*</label>
                 </span>
-                <span class="flex flex-col w-1/3 justify-between">
-                    <input type="text" readonly value="{{ $serviceProvider->contact_details }}" name="contact_details" id="contact_details" class="border-2 border-gray-300 rounded-md px-2 py-1">
-                    <label for="contact_details" class="text-sm text-gray-400 text-center">Contact Details*</label>
+                <span class="d-flex flex-column w-25 justify-content-between">
+                    <input type="text" readonly value="{{ $serviceProvider->contact_details }}" name="contact_details" id="contact_details" class="form-control">
+                    <label for="contact_details" class="form-label">Contact Details*</label>
                 </span>
             </div>
-            <div class="flex justify-between items-start w-full gap-2">
-                <span class="flex flex-col w-full justify-between">
-                    <input type="text" required value="{{ $serviceProvider->address }}" name="address" id="address" class="border-2 border-gray-300 rounded-md px-2 py-1">
-                    <label for="address" class="text-sm text-gray-400 text-center">Address(Lot No., Building No., Street)*</label>
+            <div class="d-flex justify-content-between align-items-start w-100 gap-1">
+                <span class="d-flex flex-column w-100 justify-content-between">
+                    <input type="text" required value="{{ $serviceProvider->address }}" name="address" id="address" class="form-control">
+                    <label for="address" class="form-label">Address(Lot No., Building No., Street)*</label>
                 </span>
-                <span class="flex flex-col w-full justify-between">
-                    <select required value="{{ $serviceProvider->barangay_id }}" name="barangay_id" id="barangay_id" class="border-2 border-gray-300 rounded-md px-2 py-1">
+                <span class="d-flex flex-column w-100 justify-content-between">
+                    <select required value="{{ $serviceProvider->barangay_id }}" name="barangay_id" id="barangay_id" class="form-select">
                         <option value="{{ $serviceProvider->barangay_id }}">{{ $serviceProvider->barangay->name }}</option>
                         @foreach ($barangays as $barangay)
                             <option value="{{ $barangay->id }}">{{ $barangay->name }}</option>
                         @endforeach
                     </select>
-                    <label for="provider_address_barangay" class="text-sm text-gray-400 text-center">Barangay*</label>
+                    <label for="provider_address_barangay" class="form-label">Barangay*</label>
                 </span>
             </div>
         </div>
-        <hr class="border-2 border-gray-700">
-        <div class="flex flex-col gap-4">
-            <h4 class="text-lg font-semibold">Remarks</h4>
-            <div class="flex justify-between items-start w-full gap-2">
-                <span class="flex flex-col w-full justify-between">
-                    <textarea type="text" rows="4" name="remarks" class="border-2 border-gray-300 rounded-md px-2 py-1">{{ $serviceProvider->remarks }}</textarea>
+        <hr class="border-1">
+        <div class="d-flex flex-column gap-2">
+            <h4 class="fw-semibold">Remarks</h4>
+            <div class="d-flex justify-content-between align-items-start w-100 gap-1">
+                <span class="d-flex flex-column w-100 justify-content-between">
+                    <textarea type="text" rows="4" name="remarks" class="form-control">{{ $serviceProvider->remarks }}</textarea>
                 </span>
             </div>
         </div>
     </div>
-    <span class="flex justify-center items-center gap-4">
-        <button type="submit" class="px-4 py-2 cursor-pointer text-white font-semibold tracking-widest uppercase text-sm bg-yellow-400 rounded-lg hover:bg-yellow-500 hover:text-black transition-colors">
-            Submit
+    <span class="d-flex justify-content-center align-items-center gap-2">
+        <button type="submit" class="btn btn-primary">
+            Update
         </button>
-        <a href="{{ route('admin.burial.providers') }}" class="px-4 py-2 text-white font-semibold tracking-widest uppercase text-sm bg-gray-700 rounded-lg hover:bg-gray-300 hover:text-black transition-colors">
+        <a href="{{ route('admin.burial.providers') }}" class="btn btn-secondary">
             Cancel
         </a>
     </span>
