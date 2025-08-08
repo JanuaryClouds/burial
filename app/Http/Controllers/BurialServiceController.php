84166caf960bdb4e446980a9b44538f848d091e1
JanuaryClouds <janmichaelcristianbcruz@gmail.com>
2025-08-08 15:09:31 +0800

    added: function to view burial service

--- app/Http/Controllers/BurialServiceController.php
+++ app/Http/Controllers/BurialServiceController.php
@@ -5,7 +5,12 @@ namespace App\Http\Controllers;
 use App\Http\Requests\BurialServiceRequest;
 use Illuminate\Http\Request;
 use Validator;
+use Illuminate\Support\Facades\Storage;
 use App\Services\BurialServiceService;
+use App\Models\BurialService;
+use App\Models\Relationship;
+use App\Models\Barangay;
+use App\Models\BurialServiceProvider;
 use App\Models\burialAssistanceRequest;
 
 class BurialServiceController extends Controller
@@ -20,6 +25,24 @@ class BurialServiceController extends Controller
         return view("admin.newBurialService");
     }
 
+    public function view($id) {
+        $service = BurialService::where("id", $id)->first();
+        $relationships = Relationship::getAllRelationships();
+        $barangays = Barangay::getAllBarangays();
+        $providers = BurialServiceProvider::getAllProviders();
+
+        $disk = 'public';
+        $folder = 'burial_images';
+        $prefix = 'burial-service-' . $id . '-';
+
+        $serviceImages = collect(Storage::disk($disk) ->files($folder))
+        ->filter(function ($filePath) use ($prefix) {
+            return str_starts_with(basename($filePath), $prefix);
+        });
+
+        return view('admin.view-service', compact('service', 'relationships', 'barangays', 'providers', 'serviceImages'));
+    }
+
     public function store(BurialServiceRequest $request) {
         $data = $request->validated();
         $validator = Validator::make($request->all(), [
@@ -32,7 +55,7 @@ class BurialServiceController extends Controller
         }
         
         $service = $this->BurialServiceService->store($data);
-        $filename = "burial-service-{$service->id}-" . time();
+        $filename = "burial-service-{$service->id}";
         
         if ($service && $request->hasFile('images')) {
             foreach ($request->file('images') as $index => $file) {
