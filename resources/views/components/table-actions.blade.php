9f1c63105184aa4c22990d62239a6e4c0ae6af5f
JanuaryClouds <janmichaelcristianbcruz@gmail.com>
2025-08-11 15:35:07 +0800

    added: more actions to table cell

--- resources/views/components/table-actions.blade.php
+++ resources/views/components/table-actions.blade.php
@@ -7,18 +7,41 @@
     class="row justify-content-center align-items-center g-2"
 >
     @if ($type === 'service')
+        @php
+            $contactType = "representative_contact";
+            $formLink = route('admin.burial.service.contact', ['id' => $data->id]);
+        @endphp
         <div class="col" title="View Service">
             <a href="{{ route('admin.burial.view', ['id' => $data->id]) }}" class="btn btn-primary">
                 <i class="fa-solid fa-arrow-up-right-from-square"></i>
             </a>
         </div>
-    @elseif ($type === 'request')
-        <div class="col" title="View Request">
-            <a href="{{ route('admin.burial.request.view', ['uuid' => $data->uuid]) }}" class="btn btn-primary">
+        
+        <div class="col">
+            <a
+                name=""
+                id=""
+                class="btn btn-secondary"
+                href="{{ route('admin.burial.service.print', ['id' => $data->id]) }}"
+                role="button"
+                ><i class="fa-solid fa-file-pdf"></i></a
+            >
+        </div>
+    @elseif ($type === 'provider')
+        @php
+            $contactType = "contact_details";
+            $formLink = route('admin.burial.provider.contact', ['id' => $data->id]);
+        @endphp
+        <div class="col" title="View Provider">
+            <a href="{{ route('admin.burial.provider.view', ['id' => $data->id]) }}" class="btn btn-primary">
                 <i class="fa-solid fa-arrow-up-right-from-square"></i>
             </a>
         </div>
-    @elseif ($type === 'provider')
+    @elseif ($type === 'request')
+        @php
+            $contactType = "representative_details";
+            $formLink = route('admin.burial.request.contact', ['uuid' => $data->uuid]);
+        @endphp
         <div class="col" title="View Request">
             <a href="{{ route('admin.burial.request.view', ['uuid' => $data->uuid]) }}" class="btn btn-primary">
                 <i class="fa-solid fa-arrow-up-right-from-square"></i>
@@ -27,9 +50,87 @@
     @endif
 
     <div class="col">
+        <!-- Modal trigger button -->
+        <button
+            type="button"
+            class="btn btn-secondary"
+            data-bs-toggle="modal"
+            data-bs-target="#messageModal"
+        >
+            <i class="fa-solid fa-message"></i>
+        </button>
         
-    </div>
-    <div class="col">
+        <!-- Modal Body -->
+        <!-- if you want to close by clicking outside the modal, delete the last endpoint:data-bs-backdrop and data-bs-keyboard -->
+        <div
+            class="modal fade"
+            id="messageModal"
+            tabindex="-1"
+            data-bs-backdrop="static"
+            data-bs-keyboard="false"
+            
+            role="dialog"
+            aria-labelledby="modalTitleId"
+            aria-hidden="true"
+        >
+            <div
+                class="modal-dialog modal-dialog-scrollable modal-dialog-centered modal-sm"
+                role="document"
+                style="z-index: 2"
+            >
+                <div class="modal-content">
+                    <form action="{{ $formLink }}" method="post">
+                    @csrf
+                    @method('POST')
+                        <div class="modal-header">
+                            <h5 class="modal-title" id="modalTitleId">
+                                @if ($type == 'service')
+                                    Contact Person
+                                @elseif ($type == 'request')
+                                    Contact Requester
+                                @elseif ($type == 'provider')
+                                    Contact Provider
+                                @endif
+                            </h5>
+                            <button
+                                type="button"
+                                class="btn-close"
+                                data-bs-dismiss="modal"
+                                aria-label="Close"
+                            ></button>
+                        </div>
+                        <div class="modal-body">
+                                <p class=text-body>Insert the message below</p>
+                                <textarea 
+                                    name="message" 
+                                    id="message" 
+                                    rows="5" 
+                                    max="255"
+                                    class="form-control"
+                                ></textarea>
+                            </div>
+                            <div class="modal-footer">
+                                <button
+                                type="button"
+                                class="btn btn-secondary"
+                                data-bs-dismiss="modal"
+                                >
+                                Cancel
+                            </button>
+                            <button type="submit" class="btn btn-primary">Send</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+        
+        <!-- Optional: Place to the bottom of scripts -->
+        <script>
+            const myModal = new bootstrap.Modal(
+                document.getElementById("modalId"),
+                options,
+            );
+        </script>
         
     </div>
 </div>
