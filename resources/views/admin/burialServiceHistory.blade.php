a1b22468da8538d3bb3f75b0c9ad09cf0835ab9d
JanuaryClouds <janmichaelcristianbcruz@gmail.com>
2025-08-13 11:03:04 +0800

    updated: changed service to export xlsx instead of csv

--- resources/views/admin/burialServiceHistory.blade.php
+++ resources/views/admin/burialServiceHistory.blade.php
@@ -14,10 +14,10 @@
                 name=""
                 id=""
                 class="btn btn-outline-primary d-flex align-items-center gap-2"
-                href="{{ route('admin.burial.service.csv') }}"
+                href="{{ route('admin.burial.service.xlsx') }}"
                 role="button"
                 target="_blank"
-                ><i class="fa-solid fa-file-csv"></i>Export to CSV</a
+                ><i class="fa-solid fa-file-excel"></i>Export to XLSX</a
             >
             <a href="{{ route('admin.burial.new') }}" class="btn btn-primary d-flex gap-2 align-items-center">
                 <i class="fa-solid fa-plus"></i>
