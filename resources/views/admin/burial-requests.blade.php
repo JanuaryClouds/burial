ae7a93b6602ea462484ac89bb0df90f6cba2b498
JanuaryClouds <janmichaelcristianbcruz@gmail.com>
2025-08-13 11:02:37 +0800

    added: button to export requests to xlsx

--- resources/views/admin/burial-requests.blade.php
+++ resources/views/admin/burial-requests.blade.php
@@ -14,8 +14,16 @@
         <div
             class="row justify-content-start d-flex flex-column g-2"
         >
-            <div class="col">
+            <div class="col d-flex justify-content-between gap-2">
                 <h3>Burial Requests</h3>
+                <a
+                    name=""
+                    id=""
+                    class="btn btn-outline-primary d-flex align-items-center gap-2"
+                    href="{{ route('admin.burial.request.xlsx') }}"
+                    role="button"
+                    ><i class="fa-solid fa-file-excel"></i>Export to XLSX</a
+                >
             </div>
             <div class="col">
                 <x-burial-assistance-requests-table />
