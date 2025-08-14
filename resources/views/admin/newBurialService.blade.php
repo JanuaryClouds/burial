239f87b84a072ba762f4687caa43be58d22402e9
JanuaryClouds <janmichaelcristianbcruz@gmail.com>
2025-08-14 14:10:25 +0800

    updated: hide unnecessary header

--- resources/views/admin/newBurialService.blade.php
+++ resources/views/admin/newBurialService.blade.php
@@ -2,7 +2,7 @@
 @section('content')
 <title>New Burial Service</title>
 <div class="container flex-column align-items-center gap-2">
-    <h2 class="fw-bold text-center">New Burial Service</h2>
+    <!-- <h2 class="g-0 bg-white p-3 rounded shadow-sm w-100">New Burial Service</h2> -->
 
     <x-burial-service-form />
 </div>
