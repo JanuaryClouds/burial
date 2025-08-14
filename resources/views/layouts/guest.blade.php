6f3b22fe867213e06bd0c2168e0cd9e21c9b56ea
JanuaryClouds <janmichaelcristianbcruz@gmail.com>
2025-08-14 14:13:21 +0800

    added: toast notifcation for guest

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -16,6 +16,52 @@
 style="background: url('{{ asset('images/cover.webp') }}') no-repeat center center / cover;"
 >
     @yield('content')
+
+    <div aria-live="polite" aria-atomic="true" class="position-fixed bottom-0 end-0 p-3 d-flex justify-content-end" style="z-index: 2;">
+    @if (session()->has('success'))
+        <div class="toast bg-success text-black" role="alert" aria-live="assertive" aria-atomic="true">
+            <div class="toast-header bg-success gap-2 align-items-center">
+                <i class="fa-solid fa-circle-check"></i>
+                <strong class="me-auto">Success</strong>
+                <button type="button" class="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>
+            </div>
+            <div class="toast-body">
+                {{ session('success') }}
+            </div>
+        </div>
+    @elseif (session()->has('error'))
+        <div class="toast bg-danger text-white" role="alert" aria-live="assertive" aria-atomic="true">
+            <div class="toast-header bg-danger gap-2 align-items-center">
+                <i class="fa-solid fa-xmark"></i>
+            <strong class="me-auto">Failed</strong>
+                <button type="button" class="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>
+            </div>
+            <div class="toast-body">
+                {{ session('failed') }}
+            </div>
+        </div>
+    @elseif (session()->has('info'))
+        <div class="toast bg-info text-black" role="alert" aria-live="assertive" aria-atomic="true">
+            <div class="toast-header bg-info gap-2 align-items-center">
+                <i class="fa-solid fa-xmark"></i>
+            <strong class="me-auto">Failed</strong>
+                <button type="button" class="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>
+            </div>
+            <div class="toast-body">
+                {{ session('info') }}
+            </div>
+        </div>
+    @endif
+    </div>
+    <script>
+        document.addEventListener('DOMContentLoaded', function () {
+            const toastEl = document.querySelector('.toast');
+            if (toastEl) {
+                new bootstrap.Toast(toastEl).show();
+            }
+        });
+    </script>
+
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/js/bootstrap.bundle.min.js" integrity="sha384-ndDqU0Gzau9qJ1lfW4pNLlhNTkCfHzAVBReH9diLvGRem5+R9g2FzA8ZGN954O5Q" crossorigin="anonymous"></script>
 </body>
 </html>
