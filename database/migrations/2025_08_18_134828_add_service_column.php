78c4a646545d1b67b9ad4714106a9d2f98de5715
JanuaryClouds <janmichaelcristianbcruz@gmail.com>
2025-08-18 15:34:08 +0800

    added: migration to add service_id column

---
+++ database/migrations/2025_08_18_134828_add_service_column.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('burial_assistance_requests', function (Blueprint $table) {
+            $table->foreignId('service_id')
+            ->nullable()
+            ->after('type_of_assistance')
+            ->constrained('burial_services')
+            ->nullOnDelete();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('burial_assistance_requests', function (Blueprint $table) {
+            Schema::dropColumns('burial_assistance_requests', ['service_id']);            
+        });
+    }
+};
