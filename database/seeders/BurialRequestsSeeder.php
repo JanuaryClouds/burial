b531c339fa59fd340b2643437e28b62c083b502c
JanuaryClouds <janmichaelcristianbcruz@gmail.com>
2025-08-19 10:23:19 +0800

    added: seeder for reqeusts and service

---
+++ database/seeders/BurialRequestsSeeder.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Barangay;
+use App\Models\Relationship;
+use App\Models\BurialAssistanceRequest;
+use Str;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class BurialRequestsSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $barangays = Barangay::pluck('id');
+        $relationships = Relationship::pluck('id');
+        $sampleBurialRequests = [
+            [
+                'uuid' => Str::uuid()->toString(),
+                'deceased_lastname' => 'Dela Cruz',
+                'deceased_firstname' => 'Juan',
+                'representative' => 'Representative Name',
+                'representative_contact' => '09123456789',
+                'rep_relationship' => $relationships->random(),
+                'burial_address' => '123 Main St',
+                'barangay_id' => $barangays->random(),
+                'start_of_burial' => '2025-08-25 00:00:00',
+                'end_of_burial' => '2025-08-28 00:00:00',
+                'type_of_assistance' => 8,
+                'status' => 'pending',
+                'service_id' => null,
+                'remarks' => 'Seeder Generated',
+            ],
+            [
+                'uuid' => Str::uuid()->toString(),
+                'deceased_lastname' => 'Delos Santos',
+                'deceased_firstname' => 'Fe',
+                'representative' => 'Representative Name',
+                'representative_contact' => '09987654321',
+                'rep_relationship' => $relationships->random(),
+                'burial_address' => '123 Second St',
+                'barangay_id' => $barangays->random(),
+                'start_of_burial' => '2025-08-28 00:00:00',
+                'end_of_burial' => '2025-08-30 00:00:00',
+                'type_of_assistance' => 8,
+                'status' => 'approved',
+                'service_id' => null,
+                'remarks' => 'Seeder Generated',
+            ],
+            [
+                'uuid' => Str::uuid()->toString(),
+                'deceased_lastname' => 'Mantos',
+                'deceased_firstname' => 'Romeo',
+                'representative' => 'Representative Name',
+                'representative_contact' => '09123456789',
+                'rep_relationship' => $relationships->random(),
+                'burial_address' => '123 Third St',
+                'barangay_id' => $barangays->random(),
+                'start_of_burial' => '2025-08-22 00:00:00',
+                'end_of_burial' => '2025-08-25 00:00:00',
+                'type_of_assistance' => 8,
+                'status' => 'rejected',
+                'service_id' => null,
+                'remarks' => 'Seeder Generated',
+            ],
+        ];
+
+        foreach ($sampleBurialRequests as $request) {
+            BurialAssistanceRequest::firstOrCreate($request);
+        }
+    }
+}
---
+++ database/seeders/BurialServiceSeeder.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Relationship;
+use App\Models\Barangay;
+use App\Models\BurialService;
+use App\Models\BurialServiceProvider;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class BurialServiceSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $barangays = Barangay::pluck('id');
+        $relationships = Relationship::pluck('id');
+        $providers = BurialServiceProvider::pluck('id');
+        $sampleBurialServices = [
+          [
+                'deceased_lastname' => 'Dela Cruz',
+                'deceased_firstname' => 'Juan',
+                'representative' => 'Representative Name',
+                'representative_contact' => '09123456789',
+                'rep_relationship' => $relationships->random(),
+                'burial_address' => '123 Main St',
+                'barangay_id' => $barangays->random(),
+                'start_of_burial' => '2025-08-25 00:00:00',
+                'end_of_burial' => '2025-08-28 00:00:00',
+                'burial_service_provider' => $providers->random(),
+                'collected_funds' => '1000',
+                'remarks' => 'Seeder Generated',
+          ], 
+          [
+                'deceased_lastname' => 'Dela Rosa',
+                'deceased_firstname' => 'Maria',
+                'representative' => 'Representative Name',
+                'representative_contact' => '09987654321',
+                'rep_relationship' => $relationships->random(),
+                'burial_address' => '123 Second St',
+                'barangay_id' => $barangays->random(),
+                'start_of_burial' => '2025-08-19 00:00:00',
+                'end_of_burial' => '2025-08-21 00:00:00',
+                'burial_service_provider' => $providers->random(),
+                'collected_funds' => '1125',
+                'remarks' => 'Seeder Generated',
+          ], 
+          [
+                'deceased_lastname' => 'Santos',
+                'deceased_firstname' => 'Brie',
+                'representative' => 'Representative Name',
+                'representative_contact' => '09567890123',
+                'rep_relationship' => $relationships->random(),
+                'burial_address' => '123 Third St',
+                'barangay_id' => $barangays->random(),
+                'start_of_burial' => '2025-08-26 00:00:00',
+                'end_of_burial' => '2025-08-29 00:00:00',
+                'burial_service_provider' => $providers->random(),
+                'collected_funds' => '1500',
+                'remarks' => 'Seeder Generated',
+          ], 
+        ];
+
+        foreach ($sampleBurialServices as $service) {
+            BurialService::firstOrCreate($service);
+        }
+    }
+}
