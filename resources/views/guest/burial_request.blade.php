99643f6a2fdf0042c5332a027287a85fc30fea1b
JanuaryClouds <janmichaelcristianbcruz@gmail.com>
2025-08-29 08:37:07 +0800

    updated: use alert instead of list in request form errors

--- resources/views/guest/burial_request.blade.php
+++ resources/views/guest/burial_request.blade.php
@@ -1,22 +1,19 @@
 @extends('layouts.guest')
 @section('content')
-                <title>CSWDO Burial Assistance Form</title>
-
-                                <!-- <a href="/" class="d-flex justify-content-between align-items-center fixed-top text-decoration-none btn btn-secondary m-4" style="width: 6em;">
-                                                <i class="fa-solid fa-arrow-left"></i>
-                                                Back
-                                </a> -->
-                                
-                                <div class="container d-flex justify-content-center align-middle p-5">
-                                                @if ($errors->any())
-                                                <div class="text-red-600 text-sm mb-4">
-                                                                <ul class="list-disc pl-5">
-                                                                                @foreach ($errors->all() as $error)
-                                                                                <li>{{ $error }}</li>
-                                                                                @endforeach
-                                                                </ul>
-                                                </div>
-                                                @endif
-                                                <x-burial-assistance-request-form />
-                                </div>
+   <title>CSWDO Burial Assistance Form</title>
+   <div class="container d-flex flex-column justify-content-center align-items-center px-auto m-5">
+      @if ($errors->any())
+         <div
+            class="alert alert-warning"
+            role="alert"
+         >
+            <ul class="list-group list-group-numbered">
+               @foreach ($errors->all() as $error)
+                  <li>{{ $error }}</li>
+               @endforeach
+            </ul>
+         </div>
+      @endif
+      <x-burial-assistance-request-form />
+   </div>
 @endsection
