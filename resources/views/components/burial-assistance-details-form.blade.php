d6019bc73185f03d1a19ab6947bfcec7264b89be
JanuaryClouds <janmichaelcristianbcruz@gmail.com>
2025-09-05 13:04:59 +0800

    added: burial assistance details form component

---
+++ resources/views/components/burial-assistance-details-form.blade.php
@@ -0,0 +1,35 @@
+@props([
+    "burialAssistance",
+    "readonly" => false,
+    "disabled" => false
+])
+<div
+    class="container bg-white shadow rounded p-4"
+>
+    <h2>Additional Details</h2>
+    <div
+        class="row flex-column justify-content-center align-items-end g-2"
+    >
+        <div class="col">
+            <x-form-input 
+                name="burial_assistance[funeraria]"
+                label="Funeraria"
+                required="true"
+                placeholder="Funeraria"
+                value="{{ $burialAssistance->funeraria ?? '' }}"
+                readonly="{{ $readonly }}"
+                disabled="{{ $disabled }}"
+            />
+        </div>
+        <div class="col">
+            <x-form-textarea 
+                name="burial_assistance[remarks]"
+                label="Remarks"
+                placeholder="Additional comments can be inserted here."
+                value="{{ $burialAssistance->remarks ?? '' }}"
+                readonly="{{ $readonly }}"
+                disabled="{{ $disabled }}"
+            />
+        </div>
+    </div>
+</div>
