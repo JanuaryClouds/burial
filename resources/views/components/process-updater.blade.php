da81a59c917dbde109ac1814a1888e5d13b57c76
JanuaryClouds <janmichaelcristianbcruz@gmail.com>
2025-09-12 15:27:28 +0800

    added: proper sorting of processLogs into UI

--- resources/views/components/process-updater.blade.php
+++ resources/views/components/process-updater.blade.php
@@ -1,13 +1,13 @@
 @php
-    $lastStep = $processLogs->last()?->workflowStep;
+    $processLogs = $processLogs->sortBy(fn($log) => $log->loggable?->created_at)->values();
+    $lastStep = $processLogs->last()?->loggable;
     $schema = $lastStep?->extra_data_schema ? json_decode($lastStep->extra_data_schema, true) : [];
 @endphp
-
 <div id="add-process" class="modal fade flex justify-content-center" tabindex="-1" role="dialog" aria-labelledby="add-proces" aria-hidden="true">
     @foreach ($workflowSteps as $step)
-        @if ($processLogs->count() == 0 || ($step->order_no > $processLogs->last()->workflowStep->order_no))
+        @if ($processLogs->count() == 0 || ($step?->order_no > $processLogs->first()->loggable?->order_no))
             <div class="modal-dialog" role="document">
-                <form action="{{ route('admin.application.addLog', ['status' => $application->status, 'id' => $application->id, 'step' => $step->id]) }}" method="post">
+                <form action="{{ route('admin.application.addLog', ['id' => $application->id, 'stepId' => $step->id]) }}" method="post">
                 @csrf
                     <div class="modal-content">
                         <div class="modal-header">
@@ -19,44 +19,47 @@
                         <div class="modal-body">
                             <section class="section">
                                 <div class="section-title">
-                                    <h6 class="text-muted">Previous Step: {{ $processLogs->last()->workflowStep->description ?? 'Submitted at ' . $application->application_date }}</h6>
+                                    <h6 class="text-muted">Previous Step: {{ $processLogs->first()->loggable?->description ?? 'Submitted at ' . $application->application_date }}</h6>
                                 </div>
                             </section>
                             <section class="section">
                                 <div class="section-title">
                                     <h6>Next Step: {{ $step->description }}</h6>
-                                        <div class="row">
-                                            <div class="form-group col-lg-6 col-sm-12">
-                                                <x-form-input type="date" name="date_out" id="date_out" label="Date Out" />
-                                            </div>
-                                            <div class="form-group col-lg-6 col-sm-12">
-                                                <x-form-input type="date" name="date_in" id="date_in" label="Date In" :required="true"/>
-                                            </div>
+                                    <div class="row mt-4">
+                                        <div class="form-group col-lg-6 col-sm-12 mb-0">
+                                            <x-form-input type="date" name="date_out" id="date_out" label="Date Out" />
                                         </div>
-                                        @foreach ($schema as $key => $field)
-                                            @if(is_string($field))
-                                                <div class="form-group col-lg-6 col-sm-12">
-                                                    <label for="">{{ ucfirst(str_replace('_', ' ', $key)) }}</label>
-                                                    <input 
-                                                        type="{{ $field }}"
-                                                        class="form-control"
-                                                        name="extra[{{ $key }}]"
-                                                        required
-                                                    >
-                                                </div>
-                                            @elseif (is_array($field))
-                                                @foreach ($field as $subkey => $subField)
-                                                    <div class="form-group col-lg-6 col-sm-12">
-                                                        <label for="">{{ ucfirst(str_replace('_', ' ', $subkey)) }}</label>
-                                                        <input type="{{ $subField }}" class="form-control" name="extra[{{ $key }}][{{ $subkey }} }}" required>
-                                                    </div>
-                                                @endforeach
-                                            @endif
+                                        <div class="form-group col-lg-6 col-sm-12 mb-0">
+                                            <x-form-input type="date" name="date_in" id="date_in" label="Date In" :required="true"/>
+                                        </div>
+                                    </div>
+                                </div>
+                                @php
+                                    $schema = $step->extra_data_schema ? json_decode($step->extra_data_schema, true) : [];
+                                @endphp
+                                @foreach ($schema as $key => $field)
+                                    @if(is_string($field))
+                                        <div class="form-group col-lg-6 col-sm-12 p-0">
+                                            <label for="">{{ ucfirst(str_replace('_', ' ', $key)) }}</label>
+                                            <input 
+                                                type="{{ $field }}"
+                                                class="form-control"
+                                                name="extra[{{ $key }}]"
+                                                required
+                                            >
+                                        </div>
+                                    @elseif (is_array($field))
+                                        @foreach ($field as $subkey => $subField)
+                                            <div class="form-group col-lg-6 col-sm-12">
+                                                <label for="">{{ ucfirst(str_replace('_', ' ', $subkey)) }}</label>
+                                                <input type="{{ $subField }}" class="form-control" name="extra[{{ $key }}][{{ $subkey }} }}" required>
+                                            </div>
                                         @endforeach
-                                        <div class="form-group">
-                                            <label for="comments">Comments</label>
-                                            <textarea id="comments" class="form-control" name="comments" rows="3"></textarea>
-                                        </div>
+                                    @endif
+                                @endforeach
+                                <div class="form-group">
+                                    <label for="comments">Comments</label>
+                                    <textarea id="comments" class="form-control" name="comments" rows="3"></textarea>
                                 </div>
                             </section>
                         </div>
