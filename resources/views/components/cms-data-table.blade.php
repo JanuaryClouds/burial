6649e70dd87fc7dbff46ad0aa9fb2706afa8b28d
JanuaryClouds <janmichaelcristianbcruz@gmail.com>
2025-09-19 14:51:24 +0800

    added: dynamic datatable for cms

---
+++ resources/views/components/cms-data-table.blade.php
@@ -0,0 +1,111 @@
+@props([
+    'data',
+    'type',
+])
+<div class="table-responsive">
+    <div class="dataTables_wrapper container-fluid">
+        <table id="cms-table" class="table data-table" style="width:100%">
+            <thead>
+                <tr role="row">
+                    @foreach ($data->first()->getAttributes() as $column => $value)
+                        @if (!in_array($column, ['created_at','updated_at', 'extra_data_schema', 'is_optional', 'requires_extra_data']))
+                            <th class="sorting sort-handler">{{ Str::replace('_', ' ', Str::title($column)) }}</th>
+                        @endif
+                    @endforeach
+                    <th>Actions</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($data as $entry)
+                    <tr class="bg-white">
+                        @foreach ($entry->getAttributes() as $key =>$value)
+                            @if(!in_array($key, ['created_at','updated_at', 'extra_data_schema', 'is_optional', 'requires_extra_data']))
+                                <td>{{ $value }}</td>
+                            @endif
+                        @endforeach
+                        <td>
+                            <button class="btn btn-primary" type="button" data-toggle="modal" data-target="#edit-modal-{{ $entry->id }}">
+                               <i class="fas fa-edit"></i> 
+                            </button>
+                            @if (!Request::routeIs('superadmin.cms.workflow') && !Request::routeIs('superadmin.cms.handlers'))
+                                <button class="btn btn-danger" type="button" data-toggle="modal" data-target="#delete-modal-{{ $entry->id }}">
+                                    <i class="fas fa-trash"></i>
+                                </button>
+                            @endif
+                        </td>
+                    </tr>
+                    
+                    <!-- Edit content modal -->
+                    <div id="edit-modal-{{ $entry->id }}" class="modal fade" tabindex="-1" role="dialog" aria-labelledby="edit-modal-{{ $entry->id }}" aria-hidden="true">
+                        <div class="modal-dialog modal-dialog-centered" role="document">
+                            <form action="{{ route('superadmin.cms.update', ['type' => $type, 'id' => $entry->id]) }}" method="post">
+                                @csrf
+                                <div class="modal-content">
+                                    <div class="modal-header">
+                                        <h5 class="modal-title" id="edit-modal-{{ $entry->id }}-title">Edit {{ $entry->name }} details</h5>
+                                        <button class="close" data-dismiss="modal" aria-label="Close">
+                                            <span aria-hidden="true">&times;</span>
+                                        </button>
+                                    </div>
+                                    <div class="modal-body">
+                                        @foreach ($entry->getAttributes() as $field => $value)
+                                            @if(in_array($field, ['name', 'description', 'department']))
+                                                <x-form-input
+                                                    name="{{ $field }}"
+                                                    id="{{ $field }}"
+                                                    value="{{ $value }}"
+                                                    type="text"
+                                                    label="{{ Str::replace('_',' ',ucfirst($field)) }}"
+                                                />
+                                            @endif
+                                        @endforeach  
+                                    </div>
+                                    <div class="modal-footer">
+                                        <button class="btn btn-primary" type="submit">
+                                            <i class="fas fa-save"></i>
+                                            Save
+                                        </button>
+                                        <button class="btn btn-secondary" type="button" data-dismiss="modal">
+                                            <i class="fas fa-times"></i>
+                                            Cancel
+                                        </button>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+
+                    <!-- Delete content modal -->
+                    <div id="delete-modal-{{ $entry->id }}" class="modal fade" tabindex="-1" role="dialog" aria-labelledby="delete-modal-{{ $entry->id }}" aria-hidden="true">
+                        <div class="modal-dialog modal-dialog-centered" role="document">
+                            <form action="{{ route('superadmin.cms.delete', ['type' => $type, 'id' => $entry->id]) }}" method="post">
+                                @csrf
+                                <div class="modal-content">
+                                    <div class="modal-header">
+                                        <h5 class="modal-title" id="delete-modal-{{ $entry->id }}-title">Delete {{ $entry->name }}</h5>
+                                        <button class="close" data-dismiss="modal" aria-label="Close">
+                                            <span aria-hidden="true">&times;</span>
+                                        </button>
+                                    </div>
+                                    <div class="modal-body">
+                                        <p>Are you sure you want to delete {{ $entry->name }} in the database? This will affect data connected to this.</p>
+                                    </div>
+                                    <div class="modal-footer">
+                                        <button class="btn btn-danger" type="submit">
+                                            <i class="fas fa-trash"></i>
+                                            Confirm Deletion
+                                        </button>
+                                        <button class="btn btn-secondary" data-dismiss="modal" aria-label="Close">
+                                            <i class="fas fa-times-circle"></i>
+                                            Cancel
+                                        </button>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
+</div>
