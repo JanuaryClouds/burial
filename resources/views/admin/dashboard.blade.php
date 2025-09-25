54e046b8b19c8fce2696bbf4c4105867e696ddbc
JanuaryClouds <janmichaelcristianbcruz@gmail.com>
2025-09-25 15:31:36 +0800

    added: dataTable

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -7,66 +7,16 @@
             <h1>Dashboard</h1>
         </div>
     </section>
+    <!-- TODO: Add Count cards -->
     <section class="section">
         <div class="section-title">Activity</div>
         <div class="section-body">
             <div class="row">
                 <div class="col-12 col-md-12 col-lg-4">
-                    <div class="card">
-                        <div class="card-header">
-                            <h4>Last updates you added</h4>
-                        </div>
-                        <div class="card-body">
-                            <div class="d-flex flex-column">
-                                @if ($lastLogs->isEmpty())
-                                    <p class="text-muted">No Activity as of yet</p>
-                                @else
-                                    @foreach ($lastLogs as $log)
-                                        <ul class="list-unstyled">
-                                            <div class="media mb-2 pb-2 border-bottom">
-                                                <div class="media-body">
-                                                    <span class="float-right text-muted">{{ $log->created_at->diffForHumans() }}</span>
-                                                    <a href="{{ route('admin.applications.manage', ['id' => $log->burialAssistance->id]) }}">
-                                                        <h5 class="mb-2">
-                                                            {{ $log->burialAssistance->deceased->last_name }} {{ $log->burialAssistance->deceased->first_name }}
-                                                        </h5>
-                                                    </a>
-                                                    {{ $log->loggable?->description ?? '' }}
-                                                </div>
-                                            </div>
-                                        </ul>
-                                    @endforeach
-                                @endif
-                            </div>
-                        </div>
-                    </div>
+                    <x-recent-activity :lastLogs="$lastLogs" />
                 </div>
                 <div class="col-12 col-md-12 col-lg-8">
-                    <div class="card">
-                        <div class="card-header">
-                            <h4>Pending Applications</h4>
-                        </div>
-                        <div class="card-body">
-                            @if ($pendingApplications->isEmpty())
-                                <p class="text-muted">No Pending Applications</p>
-                            @else
-                                @foreach ($pendingApplications as $pa)
-                                    <ul class="list-unstyled border-bottom">
-                                        <div class="media mb-2">
-                                            <div class="media-body">
-                                                <a href="{{ route('admin.applications.manage', ['id' => $pa->id]) }}">
-                                                    <h5 class="mb-2">
-                                                        {{ $pa->deceased->last_name }} {{ $pa->deceased->first_name }}
-                                                    </h5>
-                                                </a>
-                                                {{ $pa->created_at->diffForHumans() }}
-                                            </div>
-                                        </div>
-                                    </ul>
-                                @endforeach
-                            @endif
-                        </div>
-                    </div>
+                    <x-pending-applications-list :pendingApplications="$pendingApplications" />
                 </div>
             </div>        
         </div>
@@ -102,6 +52,25 @@
                                 data-chart-labels='@json($monthlyActivity->pluck('month'))'
                                 data-chart-type="line"
                             ></canvas>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="section">
+        <div class="section-title">
+            Tables
+        </div>
+        <div class="section-body">
+            <div class="row">
+                <div class="col">
+                    <div class="card">
+                        <div class="card-header">
+                            <h4>Burial Assistance Applications per Barangay</h4>
+                        </div>
+                        <div class="card-body">
+                            <x-per-barangay-table :applications="$applicationsByBarangay" />
                         </div>
                     </div>
                 </div>
