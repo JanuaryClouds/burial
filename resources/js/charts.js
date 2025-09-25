4a1b1b578809c971e375280db5dfb97ed9e881e6
JanuaryClouds <janmichaelcristianbcruz@gmail.com>
2025-09-25 10:57:41 +0800

    updated: render all charts with reusable function

--- resources/js/charts.js
+++ resources/js/charts.js
@@ -1,33 +1,84 @@
 import Chart from 'chart.js';
 
-export function renderPieChart(chartData, chartId, chartLabels) {
-    const piChart = document.getElementById(chartId);
-    if (piChart) {
-        new Chart(piChart, {
-           type: 'pie', 
-           data: {
-                labels: chartLabels,
-                datasets: [{
-                    label: 'Applications',
-                    data: chartData,
-                    backgroundColor: [
-                        '#fbbf24', '#f87171', '#60a5fa', '#34d399', '#a78bfa',
-                        '#f472b6', '#38bdf8', '#4ade80', '#facc15', '#818cf8'
-                    ],
+export function checkAndRenderCharts() {
+    const canvases = document.querySelectorAll('canvas')
+
+    canvases.forEach(canvas => {
+        const chartId = canvas.id;
+        if (!chartId) return;
+
+        const chartData = JSON.parse(canvas.dataset.chartData);
+        const chartLabels = JSON.parse(canvas.dataset.chartLabels);
+        const chartTitle = canvas.dataset.chartTitle || '';
+        const chartType = canvas.dataset.chartType;
+        renderChart(chartType, chartData, chartId, chartLabels, chartTitle);
+    });
+
+    function renderChart(chartType, chartData, chartId, chartLabels, chartTitle) {
+        if (chartType === 'pie') {
+            renderPieChart(chartData, chartId, chartLabels, chartTitle);
+        } else if (chartType === 'line') {
+            renderLineChart(chartData, chartId, chartLabels, chartTitle);
+        }
+    }
+
+    function renderPieChart(chartData, chartId, chartLabels, chartTitle) {
+        const piChart = document.getElementById(chartId);
+        if (piChart) {
+            new Chart(piChart, {
+               type: 'pie', 
+               data: {
+                    labels: chartLabels,
+                    datasets: [{
+                        data: chartData,
+                        backgroundColor: [
+                            '#fbbf24', '#f87171', '#60a5fa', '#34d399', '#a78bfa',
+                            '#f472b6', '#38bdf8', '#4ade80', '#facc15', '#818cf8'
+                        ],
+                        borderWidth: 1
+                    }]
+               },
+               options: {
+                   responsive: true,
+                    legend: {
+                        display: true,
+                        position: 'bottom',
+                    },
+                    title: {
+                        display: !!chartTitle,
+                        text: chartTitle
+                    }
+               }
+            });
+        }
+    }
+
+    function renderLineChart(chartData, chartId, chartLabels, chartTitle) {
+        const lineChart = document.getElementById(chartId);
+        if (lineChart) {
+            new Chart(lineChart, {
+                type: 'line',
+                data: {
+                    labels: chartLabels,
+                    datasets: [{
+                        data: chartData,
+                        fill: false,
+                        borderColor: '#3b82f6',
+                        tension: 0.1
+                    }],
                     borderWidth: 1
-                }]
-           },
-           options: {
-               responsive: true,
-                legend: {
-                    display: true,
-                    position: 'bottom',
                 },
-                title: {
-                    display: true,
-                    text: 'Burial Assistance Requests by Barangay'
+                options: {
+                    responsive: true,
+                    legend: {
+                        display: false,
+                    },
+                    title: {
+                        display: !!chartTitle,
+                        text: chartTitle
+                    }
                 }
-           }
-        });
+            })
+        }
     }
 }
