a068d53d8c9c99751edf4b0efd5d7ae34fcd33ac
JanuaryClouds <janmichaelcristianbcruz@gmail.com>
2025-09-25 15:31:58 +0800

    update: modularized dataTable rendering

---
+++ resources/js/dataTables.js
@@ -0,0 +1,83 @@
+export function checkAndRenderDataTables() {
+    $(document).ready(function () {
+        console.log("Initializing DataTable...");
+        $('#applications-table').DataTable({
+            responsive: true,
+            ordering: true, // keep ordering functional
+            dom:
+                // First row: buttons on the left, filter on the right
+                "<'row mb-2'<'col-sm-6 d-flex align-items-center'l<'mr-3'>B><'col-sm-6 d-flex justify-content-end'f>>" +
+                // Table
+                "<'row'<'col-12'tr>>" +
+                // Bottom row: info and pagination
+                "<'row mt-2'<'col-sm-6'i><'col-sm-6 d-flex justify-content-end'p>>",
+            buttons: [
+                // 'copy',
+                // 'csv',
+                // 'excel',
+                // 'pdf',
+                // 'print'
+                {
+                    text: 'Export to Excel',
+                    action: function() {
+                        const url = window.location.href;
+                        const status = url.substring(url.lastIndexOf('/') + 1);
+                        window.location.href = '/admin/applications/' + status + '/export'
+                    }
+                }
+            ],
+            columnDefs: [
+                { orderable: false, targets: [4] } // disable sorting on the Actions column
+            ],
+            classes: {
+                sortAsc: '',     // override ascending class
+                sortDesc: '',    // override descending class
+                sortable: ''     // override neutral sortable class
+            }
+        });
+        
+        $('#cms-table').DataTable({
+            responsive: true,
+            ordering: true,
+            dom: 
+                // First row: buttons on the left, filter on the right
+                "<'row mb-2'<'col-sm-6 d-flex align-items-center'l<'mr-3'>B><'col-sm-6 d-flex justify-content-end'f>>" +
+                // Table
+                "<'row'<'col-12'tr>>" +
+                // Bottom row: info and pagination
+                "<'row mt-2'<'col-sm-6'i><'col-sm-6 d-flex justify-content-end'p>>",
+            buttons:[
+                'copy', 'csv', 'excel', 'pdf', 'print'
+            ],
+            classes: {
+                sortAsc: '',     // override ascending class
+                sortDesc: '',    // override descending class
+                sortable: ''     // override neutral sortable class 
+            }
+        })
+        
+        $('#per-barangay-table').DataTable({
+            responsive: true,
+            ordering: true,
+            rowGroup: {
+                dataSrc: 0
+            },
+            order:[[0, 'asc']],
+            dom:
+                // First row: buttons on the left, filter on the right
+                "<'row mb-2'<'col-sm-6 d-flex align-items-center'l<'mr-3'>><'col-sm-6 d-flex justify-content-end'f>>" +
+                // Table
+                "<'row'<'col-12'tr>>" +
+                // Bottom row: info and pagination
+                "<'row mt-2'<'col-sm-6'i><'col-sm-6 d-flex justify-content-end'p>>",
+            // buttons:[
+            //     'copy', 'csv', 'excel', 'pdf', 'print'
+            // ],
+            classes: {
+                sortAsc: '',     // override ascending class
+                sortDesc: '',    // override descending class
+                sortable: ''     // override neutral sortable class 
+            }
+        })
+    });
+}
