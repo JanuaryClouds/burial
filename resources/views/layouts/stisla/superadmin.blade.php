57c8bab31413be16b19ed66f0e345814c4a2a71f
JanuaryClouds <janmichaelcristianbcruz@gmail.com>
2025-09-29 11:11:43 +0800

    updated: superadmin sidebar mini to be the same as admin's

--- resources/views/layouts/stisla/superadmin.blade.php
+++ resources/views/layouts/stisla/superadmin.blade.php
@@ -1,8 +1,9 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}"
-    x-data="{ sidebarMini: $persist(true), screenSmall: window.innerWidth < 1024}"
-    x-init="window.addEventListener('resize', () => {screenSmall = window.innerWidth < 1024;})">
-
+<html 
+    lang="{{ str_replace('_', '-', app()->getLocale()) }}"
+    x-data="{ sidebarMini: $persist(true), screenSmall: window.innerWidth < 1400}"
+    x-init="window.addEventListener('resize', () => {screenSmall = window.innerWidth < 1400;})"
+>
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
