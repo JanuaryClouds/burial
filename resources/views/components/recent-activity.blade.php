bbafe52541b4b6c84b941a23a94dc0e008e6bfb7
JanuaryClouds <janmichaelcristianbcruz@gmail.com>
2025-09-29 11:10:26 +0800

    updated: superadmin only elements

--- resources/views/components/recent-activity.blade.php
+++ resources/views/components/recent-activity.blade.php
@@ -1,7 +1,11 @@
 @props(['lastLogs' => []])
 <div class="card">
     <div class="card-header">
-        <h4>Last updates you added</h4>
+        @if (auth()->user()->isAdmin())
+            <h4>Last updates you added</h4>
+        @else
+            <h4>Recent Activity</h4>
+        @endif
     </div>
     <div class="card-body">
         <div class="d-flex flex-column">
@@ -13,16 +17,29 @@
                         <div class="media mb-2 pb-2 border-bottom">
                             <div class="media-body">
                                 <span class="float-right text-muted">{{ $log->created_at->diffForHumans() }}</span>
-                                <a href="{{ route('admin.applications.manage', ['id' => $log->burialAssistance->id]) }}">
+                                @if (auth()->user()->isAdmin())
+                                    <a href="{{ route('admin.applications.manage', ['id' => $log->burialAssistance->id]) }}">
+                                        <h5 class="mb-2">
+                                            {{ $log->loggable?->description ?? '' }}
+                                        </h5>
+                                    </a>
+                                @else
                                     <h5 class="mb-2">
-                                        {{ $log->loggable?->description ?? '' }}
+                                        {{ $log->burialAssistance->deceased->last_name }}, {{ $log->burialAssistance->deceased->first_name }}
                                     </h5>
-                                </a>
+                                @endif
                                 <span class="d-flex align-items-baseline justify-content-between mb-2">
-                                    <span>
-                                        <i class="fas fa-user mr-1"></i>
-                                        {{ $log->burialAssistance->deceased->last_name }} {{ $log->burialAssistance->deceased->first_name }}
-                                    </span>
+                                    @if (auth()->user()->isAdmin())
+                                        <span>
+                                            <i class="fas fa-user mr-1"></i>
+                                            {{ $log->burialAssistance->deceased->last_name }} {{ $log->burialAssistance->deceased->first_name }}
+                                        </span>
+                                    @else
+                                        <span>
+                                            <i class="fas fa-user-nurse mr-1"></i>
+                                            {{ $log->addedBy?->first_name }} {{ $log->addedBy?->last_name }}
+                                        </span>
+                                    @endif
                                     <span>
                                         <span class="badge badge-pill badge-info">
                                             {{ Str::ucfirst($log->burialAssistance->status) }}
