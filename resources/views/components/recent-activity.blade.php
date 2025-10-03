c95b43581cf84a228dc6764787c8b44682fae5e1
JanuaryClouds <janmichaelcristianbcruz@gmail.com>
2025-10-03 15:06:04 +0800

    updated: prevent rendering claimant changes in recent logs

--- resources/views/components/recent-activity.blade.php
+++ resources/views/components/recent-activity.blade.php
@@ -13,54 +13,56 @@
                 <p class="text-muted">No Activity as of yet</p>
             @else
                 @foreach ($lastLogs as $log)
-                    <ul class="list-unstyled">
-                        <div class="media mb-2 pb-2 border-bottom">
-                            <div class="media-body">
-                                <span class="float-right text-muted">{{ $log->created_at->diffForHumans() }}</span>
-                                @if (auth()->user()->isAdmin())
-                                    <a href="{{ route('admin.applications.manage', ['id' => $log->burialAssistance->id]) }}">
-                                        <h5 class="mb-2">
-                                            {{ $log->loggable?->description ?? '' }}
-                                        </h5>
-                                    </a>
-                                @else
-                                    <h5 class="mb-2">
-                                        {{ $log->burialAssistance->deceased->last_name }}, {{ $log->burialAssistance->deceased->first_name }}
-                                    </h5>
-                                @endif
-                                <span class="d-flex align-items-baseline justify-content-between mb-2">
+                    @if ($log->loggable && class_basename($log->loggable) === 'WorkflowStep')
+                        <ul class="list-unstyled">
+                            <div class="media mb-2 pb-2 border-bottom">
+                                <div class="media-body">
+                                    <span class="float-right text-muted">{{ $log->created_at->diffForHumans() }}</span>
                                     @if (auth()->user()->isAdmin())
-                                        <span>
-                                            <i class="fas fa-user mr-1"></i>
-                                            {{ $log->burialAssistance->deceased->first_name }} {{ $log->burialAssistance->deceased->last_name }}
-                                        </span>
+                                        <a href="{{ route('admin.applications.manage', ['id' => $log->burialAssistance->id]) }}">
+                                            <h5 class="mb-2">
+                                                {{ $log->loggable?->description ?? '' }}
+                                            </h5>
+                                        </a>
                                     @else
-                                        <span>
-                                            <i class="fas fa-user-nurse mr-1"></i>
-                                            {{ $log->addedBy?->first_name }} {{ $log->addedBy?->last_name }}
-                                        </span>
+                                        <h5 class="mb-2">
+                                            {{ $log->burialAssistance->deceased->first_name }} {{ $log->burialAssistance->deceased->last_name }}
+                                        </h5>
                                     @endif
-                                    <span>
-                                        <span class="badge badge-pill badge-info">
-                                            {{ Str::ucfirst($log->burialAssistance->status) }}
+                                    <span class="d-flex align-items-baseline justify-content-between mb-2">
+                                        @if (auth()->user()->isAdmin())
+                                            <span>
+                                                <i class="fas fa-user mr-1"></i>
+                                                {{ $log->burialAssistance->deceased->first_name }} {{ $log->burialAssistance->deceased->last_name }}
+                                            </span>
+                                        @else
+                                            <span>
+                                                <i class="fas fa-user-nurse mr-1"></i>
+                                                {{ $log->addedBy?->first_name }} {{ $log->addedBy?->last_name }}
+                                            </span>
+                                        @endif
+                                        <span>
+                                            <span class="badge badge-pill badge-info">
+                                                {{ Str::ucfirst($log->burialAssistance->status) }}
+                                            </span>
                                         </span>
                                     </span>
-                                </span>
-                                @if (class_basename($log->loggable) === 'WorkflowStep')
-                                    <div class="progress" style="height: 8px;">
-                                        <div 
-                                            class="progress-bar bg-primary" 
-                                            style="width: {{ ($log->loggable->order_no / App\Models\WorkflowStep::select('id')->count()) * 100 }}%;" 
-                                            role="progressbar" 
-                                            aria-valuenow="25" 
-                                            aria-valuemin="0" 
-                                            aria-valuemax="100"
-                                        ></div>
-                                    </div>
-                                @endif
+                                    @if (class_basename($log->loggable) == 'WorkflowStep')
+                                        <div class="progress" style="height: 8px;">
+                                            <div 
+                                                class="progress-bar bg-primary" 
+                                                style="width: {{ ($log->loggable->order_no / App\Models\WorkflowStep::select('id')->count()) * 100 }}%;" 
+                                                role="progressbar" 
+                                                aria-valuenow="25" 
+                                                aria-valuemin="0" 
+                                                aria-valuemax="100"
+                                            ></div>
+                                        </div>
+                                    @endif
+                                </div>
                             </div>
-                        </div>
-                    </ul>
+                        </ul>
+                    @endif
                 @endforeach
             @endif
         </div>
