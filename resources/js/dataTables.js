1f29b1dbbcab1b26224090af59580c47dc77ed84
JanuaryClouds <janmichaelcristianbcruz@gmail.com>
2025-10-28 09:21:33 +0800

    added: registered latest applications table

--- resources/js/dataTables.js
+++ resources/js/dataTables.js
@@ -160,6 +160,39 @@ export function checkAndRenderDataTables() {
             }
         })
 
+        $('#latest-applications-table').DataTable({
+            responsive: true,
+            ordering: true,
+            dom: 
+                // First row: buttons on the left, filter on the right
+                "<'row mb-2'<'col-sm-6 d-flex align-items-center'i<'mr-3'>><'col-sm-6 d-flex justify-content-end'f>>" +
+                // Table
+                "<'row'<'col-12'tr>>",
+                // Bottom row: info
+                // "<'row mt-2'<'col-sm-6'i>>"
+            buttons:[
+                {
+                    extend: 'excel',
+                    text: '<i class="mr-2 fas fa-file-excel"></i> Export to Excel',
+                    className: 'btn btn-primary py-1 px-3',
+                },
+                {
+                    extend: 'print',
+                    text: '<i class="mr-2 fas fa-print"></i> Print',
+                    className: 'btn btn-secondary py-1 px-3 ml-2',
+                },
+                // 'copy', 
+                // 'csv', 
+                // 'pdf',
+                // 'print'
+            ],
+            classes: {
+                sortAsc: '',     // override ascending class
+                sortDesc: '',    // override descending class
+                sortable: ''     // override neutral sortable class 
+            }
+        })
+
         $('#generic-table').DataTable({
             responsive: true,
             ordering: true,
