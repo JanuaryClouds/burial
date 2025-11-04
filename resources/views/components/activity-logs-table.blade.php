5ae5c1c58da72882e8c97fc7c9297f877a055282
JanuaryClouds <janmichaelcristianbcruz@gmail.com>
2025-11-04 10:57:12 +0800

    feat: show alert if logs are empty

--- resources/views/components/activity-logs-table.blade.php
+++ resources/views/components/activity-logs-table.blade.php
@@ -1,42 +1,46 @@
-<div class="table-responsive">
-    <div class="dataTables_wrapper container-fluid">
-        <table id="activity-logs-table" class="table data-table" style="width:100%">
-            <thead>
-                <tr role="row">
-                    @foreach ($logs->first()->getAttributes() as $column => $value)
-                        @php
-                            $excemptions = ['id', 'updated_at', 'causer_type'];
-                        @endphp
-                        @if (!in_array($column, $excemptions))
-                            @if ($column == "causer_id")
-                                <th class="sorting sort-handler">Caused By</th>
-                            @elseif ($column == "created_at")
-                                <th class="sorting sort-handler">Date & Time</th>
-                            @else
-                                <th class="">{{ Str::title(Str::replace('_', ' ', $column)) }}</th>     
-                            @endif
-                        @endif
-                    @endforeach
-                </tr>
-            </thead>
-            <tbody>
-                @foreach ($logs as $log)
-                    <tr class="bg-white">
-                        @foreach ($log->getAttributes() as $key => $value)
-                            @if (!in_array($key, $excemptions))
-                                @if (auth()->user()->hasRole('superadmin') && $key == 'causer_id')
-                                    <td>{{ auth()->user()->where('id', $value)->first()->first_name ?? '' }}</td>
+@if ($logs->count() > 0)
+    <div class="table-responsive">
+        <div class="dataTables_wrapper container-fluid">
+            <table id="activity-logs-table" class="table data-table" style="width:100%">
+                <thead>
+                    <tr role="row">
+                        @foreach ($logs->first()->getAttributes() as $column => $value)
+                            @php
+                                $excemptions = ['id', 'updated_at', 'causer_type'];
+                            @endphp
+                            @if (!in_array($column, $excemptions))
+                                @if ($column == "causer_id")
+                                    <th class="sorting sort-handler">Caused By</th>
+                                @elseif ($column == "created_at")
+                                    <th class="sorting sort-handler">Date & Time</th>
                                 @else
-                                    <td>{{ $value }}</td>
+                                    <th class="">{{ Str::title(Str::replace('_', ' ', $column)) }}</th>     
                                 @endif
                             @endif
                         @endforeach
                     </tr>
-                @endforeach
-            </tbody>
-        </table>
+                </thead>
+                <tbody>
+                    @foreach ($logs as $log)
+                        <tr class="bg-white">
+                            @foreach ($log->getAttributes() as $key => $value)
+                                @if (!in_array($key, $excemptions))
+                                    @if (auth()->user()->hasRole('superadmin') && $key == 'causer_id')
+                                        <td>{{ auth()->user()->where('id', $value)->first()->first_name ?? '' }}</td>
+                                    @else
+                                        <td>{{ $value }}</td>
+                                    @endif
+                                @endif
+                            @endforeach
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
     </div>
-</div>
+@else 
+    <div class="alert alert-info">No activity logs found.</div>
+@endif
 <script>
     document.addEventListener('DOMContentLoaded', function() {
         $('#activity-logs-table').DataTable({
--- resources/views/components/process-logs-table.blade.php
+++ resources/views/components/process-logs-table.blade.php
@@ -1,54 +1,58 @@
-<div class="table-responsive">
-    <div class="dataTables_wrapper container-fluid">
-        <table id="generic-table" class="table data-table generic-table" style="width:100%">
-            <thead>
-                <tr role="row">
-                    @foreach ($processLogs->first()->getAttributes() as $column => $value)
-                        @php
-                            $excemptions = ['id', 'updated_at', 'loggable_type'];
-                        @endphp
-                        @if (!in_array($column, $excemptions))
-                            @if ($column == "burial_assistance_id")
-                                <th class="sorting sort-handler">Burial Assistance of</th>
-                            @elseif ($column == "loggable_id")
-                                <th class="sorting sort-handler">Process</th>
-                            @else
-                                <th class="sorting sort-handler">{{ Str::title(Str::replace('_', ' ', $column)) }}</th>     
-                            @endif
-                        @endif
-                    @endforeach
-                </tr>
-            </thead>
-            <tbody>
-                @foreach ($processLogs as $log)
-                    <tr class="bg-white">
-                        @foreach ($log->getAttributes() as $key => $value)
-                            @if (!in_array($key, $excemptions))
-                                @if (auth()->user()->hasRole('superadmin') && $key == 'added_by')
-                                    <td>{{ auth()->user()->where('id', $value)->first()->first_name ?? '' }}</td>
-                                @elseif ($key == 'burial_assistance_id')
-                                    @php
-                                        $deceased = \App\Models\BurialAssistance::where('id', $value)->first()->deceased;
-                                        $fullName = $deceased ? $deceased->first_name . ' ' . Str::limit($deceased->middle_name, 1, '.') . ' ' . $deceased->last_name : ' ' . $deceased?->suffix;
-                                    @endphp
-                                    <td>{{ $fullName }}</td>
-                                @elseif ($key == 'loggable_id')
-                                    @php
-                                        if (class_basename($log->loggable) === 'WorkflowStep') {
-                                            $processName = $log->loggable->description;
-                                        } else {
-                                            $processName = $log->comments;
-                                        }
-                                    @endphp
-                                    <td>{{ $processName }}</td>
+@if ($processLogs->count() > 0)
+    <div class="table-responsive">
+        <div class="dataTables_wrapper container-fluid">
+            <table id="generic-table" class="table data-table generic-table" style="width:100%">
+                <thead>
+                    <tr role="row">
+                        @foreach ($processLogs->first()->getAttributes() as $column => $value)
+                            @php
+                                $excemptions = ['id', 'updated_at', 'loggable_type'];
+                            @endphp
+                            @if (!in_array($column, $excemptions))
+                                @if ($column == "burial_assistance_id")
+                                    <th class="sorting sort-handler">Burial Assistance of</th>
+                                @elseif ($column == "loggable_id")
+                                    <th class="sorting sort-handler">Process</th>
                                 @else
-                                    <td>{{ $value }}</td>
+                                    <th class="sorting sort-handler">{{ Str::title(Str::replace('_', ' ', $column)) }}</th>     
                                 @endif
                             @endif
                         @endforeach
                     </tr>
-                @endforeach
-            </tbody>
-        </table>
+                </thead>
+                <tbody>
+                    @foreach ($processLogs as $log)
+                        <tr class="bg-white">
+                            @foreach ($log->getAttributes() as $key => $value)
+                                @if (!in_array($key, $excemptions))
+                                    @if (auth()->user()->hasRole('superadmin') && $key == 'added_by')
+                                        <td>{{ auth()->user()->where('id', $value)->first()->first_name ?? '' }}</td>
+                                    @elseif ($key == 'burial_assistance_id')
+                                        @php
+                                            $deceased = \App\Models\BurialAssistance::where('id', $value)->first()->deceased;
+                                            $fullName = $deceased ? $deceased->first_name . ' ' . Str::limit($deceased->middle_name, 1, '.') . ' ' . $deceased->last_name : ' ' . $deceased?->suffix;
+                                        @endphp
+                                        <td>{{ $fullName }}</td>
+                                    @elseif ($key == 'loggable_id')
+                                        @php
+                                            if (class_basename($log->loggable) === 'WorkflowStep') {
+                                                $processName = $log->loggable->description;
+                                            } else {
+                                                $processName = $log->comments;
+                                            }
+                                        @endphp
+                                        <td>{{ $processName }}</td>
+                                    @else
+                                        <td>{{ $value }}</td>
+                                    @endif
+                                @endif
+                            @endforeach
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
     </div>
-</div>
+@else
+    <div class="alert alert-info">No process logs found.</div>
+@endif
