64a1c4603699baafcf3ffe6f9cc075018e59636f
JanuaryClouds <janmichaelcristianbcruz@gmail.com>
2025-11-10 21:52:35 +0800

    feat: created interview model and controller

---
+++ app/Models/Interview.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Interview extends Model
+{
+    use HasFactory;
+    protected $table = 'interviews';
+
+    protected $primaryKey = 'id';
+    public $incrementing = false;
+    protected $keyType = 'string';
+    protected $fillable = [
+        'id',
+        'client_id',
+        'status',
+        'schedule',
+        'remarks',
+    ];
+
+    public function client()
+    {
+        return $this->belongsTo(Client::class);
+    }
+}
---
+++ database/migrations/2025_11_10_113141_create_interviews_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('interviews', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+
+            $table->foreignUuid('client_id')
+                ->constrained('clients', 'id')
+                ->onDelete('cascade')
+                ->onUpdate('cascade');
+            $table->enum('status', ['scheduled', 'done']) ->default('scheduled');
+            $table->dateTime('schedule');
+            $table->string('remarks')->nullable();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('interviews');
+    }
+};
