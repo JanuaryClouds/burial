60b00c6cfe2c07b053c54bc3173889f57511b57c
JanuaryClouds <janmichaelcristianbcruz@gmail.com>
2025-11-12 11:04:45 +0800

    fix | Recommended Assistance | made amount and MoA fields conditional

--- resources/views/client/partial/recommendedAssistance.blade.php
+++ resources/views/client/partial/recommendedAssistance.blade.php
@@ -34,33 +34,44 @@
         </div>
     @endforeach
     <div class="col">
-        @include('components.form-input', [
-            'name' => 'referral',
-            'label' => 'Referral',
-            'value' => $recommendedAssistance->referral ?? null,
-            'readonly' => $recommendedAssistance
-        ])
-        @include('components.form-input', [
-            'name' => 'amount',
-            'label' => 'Amount of Assistance to be Extended',
-            'type' => 'number',
-            'value' => $recommendedAssistance->amount ?? null,
-            'readonly' => $recommendedAssistance
-        ])
-        @include('components.form-select', [
-            'name' => 'moa_id',
-            'label' => 'Mode of Assistance',
-            'options' => $modes,
-            'selected' => $recommendedAssistance->moa_id ?? null,
-            'disabled' => $recommendedAssistance
-        ])
         @include('components.form-select', [
             'name' => 'type',
+            'id' => 'type',
             'label' => 'Type of Assistance',
             'options' => $types,
             'selected' => $recommendedAssistance->type ?? null,
             'disabled' => $recommendedAssistance
         ])
+        <div id="referral">
+            @include('components.form-input', [
+                'name' => 'referral',
+                'id' => 'referralField',
+                'label' => 'Referral',
+                'value' => $recommendedAssistance->referral ?? null,
+                'readonly' => $recommendedAssistance
+            ])
+        </div>
+        <div id="amount">
+            @include('components.form-input', [
+                'name' => 'amount',
+                'id' => 'amountField',
+                'min' => 0,
+                'label' => 'Amount of Assistance to be Extended',
+                'type' => 'number',
+                'value' => $recommendedAssistance->amount ?? null,
+                'readonly' => $recommendedAssistance
+            ])
+        </div>
+        <div id="moa">
+            @include('components.form-select', [
+                'name' => 'moa_id',
+                'id' => 'moaField',
+                'label' => 'Mode of Assistance',
+                'options' => $modes,
+                'selected' => $recommendedAssistance->moa_id ?? null,
+                'disabled' => $recommendedAssistance
+            ])
+        </div>
         @include('components.form-textarea', [
             'name' => 'remarks',
             'label' => 'Remarks',
@@ -69,3 +80,29 @@
         ])
     </div>
 </div>
+<script>
+    const typeField = document.getElementById('type');
+    const referralField = document.getElementById('referralField');
+    const amountContainer = document.getElementById('amount');
+    const amountField = document.getElementById('amountField');
+    const moaContainer = document.getElementById('moa');
+    const moaField = document.getElementById('moaField');
+
+    typeField.addEventListener('change', function () {
+        if (typeField.value == 'funeral') {
+            referralField.value = "Taguig City Public Cemetery";
+            amountContainer.classList.add('d-none');
+            moaContainer.classList.add('d-none');
+            amountField.removeAttribute('required');
+            moaField.removeAttribute('required');
+        }
+        
+        if (typeField.value == 'burial') {
+            referralField.value = "";
+            amountContainer.classList.remove('d-none');
+            moaContainer.classList.remove('d-none');
+            amountField.setAttribute('required', 'true');
+            moaField.setAttribute('required', 'true');
+        }
+    })
+</script>
