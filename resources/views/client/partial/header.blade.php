a0f0fd5b13e43e42436e78f12cdfee3723f4b3b6
JanuaryClouds <janmichaelcristianbcruz@gmail.com>
2025-11-27 15:58:50 +0800

    fix | Header buttons | removed font weight

--- resources/views/client/partial/header.blade.php
+++ resources/views/client/partial/header.blade.php
@@ -13,13 +13,13 @@
                 <div class="col d-flex align-items-center justify-content-end gap-4">
                     @if (Route::is('landing.page'))
                         @if (env('APP_DEBUG') && session()->has('citizen'))
-                            <a href="{{ route('landing.page', ['uuid', 'debug']) }}"
-                                class="btn btn-danger fw-bold hover-scale">
+                            <a href="{{ route('landing.page', ['uuid' => 'debug']) }}"
+                                class="btn btn-danger  hover-scale">
                                 Clear Session
                             </a>
                         @endif
                         @if (session('citizen') && session('citizen')['user_id'])
-                            <a href="{{ route('general.intake.form') }}" class="btn btn-primary fw-bold hover-scale">
+                            <a href="{{ route('general.intake.form') }}" class="btn btn-primary  hover-scale">
                                 Apply
                             </a>
                             @if ($existingClient)
@@ -28,10 +28,19 @@
                                 </a>
                             @endif
                         @else
-                            <a href="https://tlcportal.taguig.gov.ph/login" class="btn btn-primary fw-bold hover-scale">
+                            <a href="https://tlcportal.taguig.gov.ph/login" class="btn btn-primary  hover-scale">
                                 Register
                             </a>
+                            @if (Route::is('landing.page'))
+                                <a href="{{ route('login.page') }}" class="btn btn-light  hover-scale">
+                                    Sign In
+                                </a>
+                            @endif
                         @endif
+                    @elseif (!Route::is('general.intake.form'))
+                        <a href="{{ route('landing.page') }}" class="btn btn-light">
+                            Back
+                        </a>
                     @endif
                     @includeWhen(Route::is('general.intake.form'), 'client.partial.create-form-buttons')
                 </div>
