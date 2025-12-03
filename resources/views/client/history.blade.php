1b54a567f706cf7b2c62c395c9e3c9adea54d231
JanuaryClouds <janmichaelcristianbcruz@gmail.com>
2025-12-03 12:53:08 +0800

    fix | History | counting assistances displaying incorrectly

--- resources/views/client/history.blade.php
+++ resources/views/client/history.blade.php
@@ -24,24 +24,31 @@
                 <div class="card-footer pb-0 pt-4">
                     <ul class="nav nav-tabs nav-line-tabs nav-stretch fs-4 border-bottom-0">
                         <li class="nav-item">
+                            <a href="#applications_tab" class="nav-link text-gray-700 pb-6" data-bs-toggle="tab">
+                                Applications
+                                <span
+                                    class="badge rounded-pill text-bg-light ms-2">{{ $records ? $records->count() : 0 }}</span>
+                            </a>
+                        </li>
+                        <li class="nav-item">
                             <a href="#interviews_tab" class="nav-link active text-gray-700 pb-6" data-bs-toggle="tab">
                                 Interviews
                                 <span
-                                    class="badge rounded-pill text-bg-light ms-2">{{ $records->first()->interviews ? $records->sum(fn($c) => $c->interviews->count()) : 0 }}</span>
+                                    class="badge rounded-pill text-bg-light ms-2">{{ $records->sum(fn($c) => $c->interviews?->count()) ?? 0 }}</span>
                             </a>
                         </li>
                         <li class="nav-item">
                             <a href="#burials_tab" class="nav-link text-gray-700 pb-6" data-bs-toggle="tab">
                                 Burial Assistances
                                 <span
-                                    class="badge rounded-pill text-bg-light ms-2">{{ $records->first()->claimant ? $records->sum(fn($c) => $c->claimant->burialAssistance->count()) : 0 }}</span>
+                                    class="badge rounded-pill text-bg-light ms-2">{{ $records->sum(fn($c) => $c->claimant?->burialAssistance?->count()) ?? 0 }}</span>
                             </a>
                         </li>
                         <li class="nav-item">
                             <a href="#funerals_tab" class="nav-link text-gray-700 pb-6" data-bs-toggle="tab">
                                 Funeral Assistances
                                 <span
-                                    class="badge rounded-pill text-bg-light ms-2">{{ $records->first()->funeralAssistance ? $records->sum(fn($c) => $c->funeralAssistance->count()) : 0 }}</span>
+                                    class="badge rounded-pill text-bg-light ms-2">{{ $records->sum(fn($c) => $c->funeralAssistance?->count()) ?? 0 }}</span>
                             </a>
                         </li>
                     </ul>
@@ -51,6 +58,9 @@
             <div class="card">
                 <div class="card-body">
                     <div class="tab-content" id="tabContent">
+                        <div id="applications_tab" class="tab-pane fade" role="tabpanel">
+                            @include('client.partial.application-history')
+                        </div>
                         <div id="interviews_tab" class="tab-pane fade show active" role="tabpanel">
                             @include('client.partial.interview-history')
                         </div>
