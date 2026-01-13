e750958f3be638b35c6ae5e61f42450bbaf4c12c
JanuaryClouds <janmichaelcristianbcruz@gmail.com>
2026-01-13 14:15:49 +0800

    fix | Funeral | display text if data is empty

--- resources/views/funeral/index.blade.php
+++ resources/views/funeral/index.blade.php
@@ -7,81 +7,85 @@
             <h1 class="card-title">Manage Funeral Assistances</h1>
         </div>
         <div class="card-body">
-            <div class="table-responsive overflow-x-hidden">
-                <div class="dataTables_wrapper">
-                    <table id="{{ $resource }}-table" class="table data-table" style="width:100%">
-                        <thead class="border-bottom border-bottom-1 border-gray-200 fw-bold">
-                            <tr role="row">
-                                @foreach ($data->first()->getAttributes() as $column => $value)
-                                    @if (in_array($column, $renderColumns))
-                                        @if ($column == 'client_id')
-                                            <th class="sorting sort-handler">Name</th>
-                                        @else
-                                            <th class="">
-                                                {{ str_replace('Id', '', str_replace('_', ' ', Str::title($column))) }}
-                                            </th>
-                                        @endif
-                                    @endif
-                                @endforeach
-                                <th class="sorting sort-handler">
-                                    Beneficiary
-                                </th>
-                                <th class="sorting sort-handler">
-                                    Submitted at
-                                </th>
-                                <th class="sorting sort-handler">
-                                    Status
-                                </th>
-                                <th>Actions</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            @foreach ($data as $entry)
-                                <tr class="">
-                                    @foreach ($entry->getAttributes() as $key => $value)
-                                        @if (in_array($key, $renderColumns))
-                                            @if ($key == 'client_id')
-                                                <td>
-                                                    {{ $entry->client->first_name }}
-                                                    {{ Str::limit($entry->client->middle_name, '1', '.') }}
-                                                    {{ $entry->client->last_name }}
-                                                    {{ $entry->client->suffix ?? '' }}
-                                                </td>
+            @if (!isset($data))
+                <p class="text-muted text-center">No Applications</p>
+            @else
+                <div class="table-responsive overflow-x-hidden">
+                    <div class="dataTables_wrapper">
+                        <table id="{{ $resource }}-table" class="table data-table" style="width:100%">
+                            <thead class="border-bottom border-bottom-1 border-gray-200 fw-bold">
+                                <tr role="row">
+                                    @foreach ($data->first()->getAttributes() as $column => $value)
+                                        @if (in_array($column, $renderColumns))
+                                            @if ($column == 'client_id')
+                                                <th class="sorting sort-handler">Name</th>
+                                            @else
+                                                <th class="">
+                                                    {{ str_replace('Id', '', str_replace('_', ' ', Str::title($column))) }}
+                                                </th>
                                             @endif
                                         @endif
                                     @endforeach
-                                    <td>
-                                        {{ $entry->client->beneficiary->first_name }}
-                                        {{ Str::limit($entry->client->beneficiary->middle_name, '1', '.') }}
-                                        {{ $entry->client->beneficiary->last_name }}
-                                        {{ $entry->client->beneficiary->suffix ?? '' }}
-                                    </td>
-                                    <td>
-                                        {{ $entry->client->created_at->format('M d, Y') }}
-                                    </td>
-                                    <td>
-                                        @if ($entry->approved_at == null)
-                                            <span class="badge badge-danger">Pending</span>
-                                        @else
-                                            @if ($entry->forwarded_at == null)
-                                                <span class="badge badge-warning">Approved</span>
-                                            @else
-                                                <span class="badge badge-success">Forwarded</span>
-                                            @endif
-                                        @endif
-                                    </td>
-                                    <td>
-                                        <a href="{{ route('funeral.show', ['id' => $entry->id]) }}"
-                                            class="btn btn-primary">
-                                            <i class="fas fa-eye"></i>
-                                        </a>
-                                    </td>
+                                    <th class="sorting sort-handler">
+                                        Beneficiary
+                                    </th>
+                                    <th class="sorting sort-handler">
+                                        Submitted at
+                                    </th>
+                                    <th class="sorting sort-handler">
+                                        Status
+                                    </th>
+                                    <th>Actions</th>
                                 </tr>
-                            @endforeach
-                        </tbody>
-                    </table>
+                            </thead>
+                            <tbody>
+                                @foreach ($data as $entry)
+                                    <tr class="">
+                                        @foreach ($entry->getAttributes() as $key => $value)
+                                            @if (in_array($key, $renderColumns))
+                                                @if ($key == 'client_id')
+                                                    <td>
+                                                        {{ $entry->client->first_name }}
+                                                        {{ Str::limit($entry->client->middle_name, '1', '.') }}
+                                                        {{ $entry->client->last_name }}
+                                                        {{ $entry->client->suffix ?? '' }}
+                                                    </td>
+                                                @endif
+                                            @endif
+                                        @endforeach
+                                        <td>
+                                            {{ $entry->client->beneficiary->first_name }}
+                                            {{ Str::limit($entry->client->beneficiary->middle_name, '1', '.') }}
+                                            {{ $entry->client->beneficiary->last_name }}
+                                            {{ $entry->client->beneficiary->suffix ?? '' }}
+                                        </td>
+                                        <td>
+                                            {{ $entry->client->created_at->format('M d, Y') }}
+                                        </td>
+                                        <td>
+                                            @if ($entry->approved_at == null)
+                                                <span class="badge badge-danger">Pending</span>
+                                            @else
+                                                @if ($entry->forwarded_at == null)
+                                                    <span class="badge badge-warning">Approved</span>
+                                                @else
+                                                    <span class="badge badge-success">Forwarded</span>
+                                                @endif
+                                            @endif
+                                        </td>
+                                        <td>
+                                            <a href="{{ route('funeral.show', ['id' => $entry->id]) }}"
+                                                class="btn btn-primary">
+                                                <i class="fas fa-eye"></i>
+                                            </a>
+                                        </td>
+                                    </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
+                    </div>
                 </div>
-            </div>
+            @endif
         </div>
     </div>
     <script>
