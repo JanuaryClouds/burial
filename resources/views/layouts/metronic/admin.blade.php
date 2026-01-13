24ce4e58d8046082d9288f5681f498730cd0eb32
JanuaryClouds <janmichaelcristianbcruz@gmail.com>
2026-01-13 13:15:52 +0800

    fix | Title | Use env title

--- resources/views/layouts/metronic/admin.blade.php
+++ resources/views/layouts/metronic/admin.blade.php
@@ -5,6 +5,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>{{ $page_title ?? config('app.name') }}</title>
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js"
         integrity="sha512-v2CJ7UaYy4JwqLDIrZUI/4hqeoQieOmAZNXBeQyjo21dadnwR+8ZaIJVT8EE2iyI61OV8e6M8PP2/4hpQINQ/g=="
--- resources/views/layouts/metronic/guest.blade.php
+++ resources/views/layouts/metronic/guest.blade.php
@@ -5,6 +5,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>{{ $page_title ?? config('app.name') }}</title>
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js"
         integrity="sha512-v2CJ7UaYy4JwqLDIrZUI/4hqeoQieOmAZNXBeQyjo21dadnwR+8ZaIJVT8EE2iyI61OV8e6M8PP2/4hpQINQ/g=="
@@ -27,8 +28,6 @@
 
     @vite(['resources/js/app.js', 'resources/css/app.css'])
 </head>
-
-{{-- FIXME: theme and account options do not dropdown --}}
 
 <body id="kt_body" class="app-blank"
     style="background: url('{{ asset('images/white_bg_city.png') }}') no-repeat center center / cover; overflow-x: hidden; background-attachment: fixed;">
