bb6f44f5296dcb1a05a50e04e4f20a900d0c164c
JanuaryClouds <janmichaelcristianbcruz@gmail.com>
2026-03-04 15:54:04 +0800

    fix | Services | removed file server token

--- config/services.php
+++ config/services.php
@@ -46,6 +46,5 @@ return [
     'fileserver' => [
         'url' => env('FILESERVER_URL', ''),
         'api' => env('FILESERVER_API', ''),
-        'token' => env('FILESERVER_TOKEN', ''),
     ]
 ];
