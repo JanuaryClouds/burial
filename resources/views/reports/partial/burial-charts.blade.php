59cf4d63b308ef09a89d4b6dba1241a60695d2e2
JanuaryClouds <janmichaelcristianbcruz@gmail.com>
2026-03-06 13:07:50 +0800

    chore | Burial Chart | relabeled Deceased to Beneficiary

--- resources/views/reports/partial/burial-charts.blade.php
+++ resources/views/reports/partial/burial-charts.blade.php
@@ -1,17 +1,13 @@
 <div class="col-12 col-lg-6">
     <div class="card">
         <div class="card-header">
-            <h4 class="card-title">Deceased Per Religion</h4>
+            <h4 class="card-title">Beneficiary Per Religion</h4>
         </div>
         <div class="card-body">
             <div>
-                <canvas
-                    id="deceased-per-religion"
-                    data-chart-data='@json($deceasedPerReligion->pluck('count'))'
-                    data-chart-labels='@json($deceasedPerReligion->pluck('name'))'
-                    data-chart-type="bar"
-                    data-empty="{{ $deceasedPerReligion->isEmpty() ? 'true' : 'false' }}"
-                ></canvas>
+                <canvas id="deceased-per-religion" data-chart-data='@json($deceasedPerReligion->pluck('count'))'
+                    data-chart-labels='@json($deceasedPerReligion->pluck('name'))' data-chart-type="bar"
+                    data-empty="{{ $deceasedPerReligion->isEmpty() ? 'true' : 'false' }}"></canvas>
             </div>
         </div>
     </div>
@@ -19,17 +15,13 @@
 <div class="col-12 col-lg-6">
     <div class="card">
         <div class="card-header">
-            <h4 class="card-title">Deceased Per Barangay</h4>
+            <h4 class="card-title">Beneficiary Per Barangay</h4>
         </div>
         <div class="card-body">
             <div>
-                <canvas
-                    id="deceased-per-barangay"
-                    data-chart-data='@json($deceasedPerBarangay->pluck('count'))'
-                    data-chart-labels='@json($deceasedPerBarangay->pluck('name'))'
-                    data-chart-type="bar"
-                    data-empty="{{ $deceasedPerBarangay->isEmpty() ? 'true' : 'false' }}"
-                ></canvas>
+                <canvas id="deceased-per-barangay" data-chart-data='@json($deceasedPerBarangay->pluck('count'))'
+                    data-chart-labels='@json($deceasedPerBarangay->pluck('name'))' data-chart-type="bar"
+                    data-empty="{{ $deceasedPerBarangay->isEmpty() ? 'true' : 'false' }}"></canvas>
             </div>
         </div>
     </div>
