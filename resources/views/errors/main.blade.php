d980569ea93f648a0f97c99fe79b2020eaa3e65b
JanuaryClouds <janmichaelcristianbcruz@gmail.com>
2026-03-12 23:43:13 +0800

    fix | Title | resolve issues with title on multiple pages

--- resources/views/errors/main.blade.php
+++ resources/views/errors/main.blade.php
@@ -1,17 +1,16 @@
-@extends('layouts.stisla.guest')
-<title></title>
+@extends('layouts.metronic.guest')
 @section('content')
-<div class="container d-flex justify-content-center align-items-center min-vh-100 min-vw-100">
-    <div class="card">
-        <div class="card-body">
-            <h5 class="card-title">You have encountered an error</h5>
-            <p class="card-text">Seems like you encountered an error doing your action.</p>
-        </div>
-        <div class="card-footer">
-            <a href="{{ url()->previous() }}" class="btn btn-primary">
-                Go Back
-            </a>
+    <div class="container d-flex justify-content-center align-items-center min-vh-100 min-vw-100">
+        <div class="card">
+            <div class="card-body">
+                <h5 class="card-title">You have encountered an error</h5>
+                <p class="card-text">Seems like you encountered an error doing your action.</p>
+            </div>
+            <div class="card-footer">
+                <a href="{{ url()->previous() }}" class="btn btn-primary">
+                    Go Back
+                </a>
+            </div>
         </div>
     </div>
-</div>
 @endsection
