c7df39e92f5a491b4a8822fc8589db7ec8a19681
JanuaryClouds <janmichaelcristianbcruz@gmail.com>
2026-04-01 00:05:00 +0800

    fix | Timeline | added rounded corners

--- resources/views/burial/partials/timeline.blade.php
+++ resources/views/burial/partials/timeline.blade.php
@@ -5,7 +5,7 @@
     </li>
     @foreach ($timeline as $log)
         <li
-            class="list-group-item d-flex justify-content-between align-items-center {{ $loop->last ? 'bg-primary text-white' : 'bg-body-secondary' }}">
+            class="list-group-item d-flex justify-content-between align-items-center {{ $loop->last ? 'bg-primary text-white rounded-bottom' : 'bg-body-secondary' }}">
             <p class="mb-0 {{ $loop->last ? 'fw-bold text-white' : '' }}">
                 {{ $log['step'] ? 'Step ' . $log['step'] . ':' : '' }} {{ $log['description'] ?? 'System Remark' }}
                 @if (isset($log['comments']))
@@ -36,7 +36,7 @@
                     {{ $log['out'] ? '/ Out: ' . $log['out'] : '' }}
                 </span>
                 @auth
-                    @if (auth()->user()->can('add-updates') && $loop->last && !$log['loggable'] instanceof ClaimantChange)
+                    @if (auth()->user()->can('add-updates') && $loop->last && !$log['loggable'] instanceof App\Models\ClaimantChange)
                         @include('burial.partials.delete-log', [
                             'id' => $log['id'],
                             'step' => $log['step'],
@@ -47,14 +47,14 @@
         </li>
         @if (isset($log['comments']))
             <li id="show-comments-{{ $log['id'] }}"
-                class="list-group-item border-top-0 collapse {{ $loop->last ? 'bg-primary text-white' : 'bg-body-secondary' }}">
+                class="list-group-item border-top-0 collapse {{ $loop->last ? 'bg-primary text-white rounded-bottom' : 'bg-body-secondary' }}">
                 <p class="mb-0">{{ $log['comments'] }}</p>
             </li>
         @endif
         @if (isset($log['extra_data']))
             <div id="show-extra-data-{{ $log['id'] }}" class="collapse">
                 <li
-                    class="list-group-item border-top-0 {{ $loop->last ? 'bg-primary text-white' : 'bg-body-secondary' }}">
+                    class="list-group-item border-top-0 {{ $loop->last ? 'bg-primary text-white rounded-bottom' : 'bg-body-secondary' }}">
                     @foreach ($log['extra_data'] as $key => $subKey)
                         @if (is_array($subKey))
                             @foreach ($subKey as $data => $value)
