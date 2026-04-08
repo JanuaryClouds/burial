05cd6ac438ad3634529bf663be1cfaa5cefc954b
JanuaryClouds <janmichaelcristianbcruz@gmail.com>
2026-04-09 00:58:36 +0800

    feat | Routes | implement permissions on route layer

--- routes/client.php
+++ routes/client.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\InterviewController;
 use Illuminate\Support\Facades\Route;
 
 Route::resource('client', ClientController::class)
+    ->middleware('permission:view-clients')
     ->only(['index', 'show']);
 
 Route::get('/general-intake-form', [ClientController::class, 'create'])
@@ -24,14 +25,17 @@ Route::prefix('clients')
             ->name('gis-form');
 
         Route::post('/{id}/schedule', [InterviewController::class, 'store'])
+            ->middleware('permission:create-interview-schedules')
             ->name('interview.schedule.store');
 
         Route::post('/{id}/schedule/done', [InterviewController::class, 'done'])
             ->name('interview.schedule.done');
 
         Route::post('/{id}/assessment', [ClientController::class, 'assessment'])
+            ->middleware('permission:create-assessments')
             ->name('assessment.store');
 
         Route::post('/{id}/recommendation', [ClientController::class, 'recommendedService'])
+            ->middleware('permission:create-recommendations')
             ->name('recommendation.store');
     });
