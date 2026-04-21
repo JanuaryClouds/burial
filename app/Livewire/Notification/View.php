60433a4b82f91c8a84dacf60657030d7f688d46e
JanuaryClouds <janmichaelcristianbcruz@gmail.com>
2026-04-22 01:22:28 +0800

    WIP | Notification View

---
+++ app/Livewire/Notification/View.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Livewire\Notification;
+
+use Livewire\Component;
+
+class View extends Component
+{
+
+    public function render()
+    {
+        return view('livewire.notification.view');
+    }
+}
