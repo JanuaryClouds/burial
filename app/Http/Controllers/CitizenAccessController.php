a4f3f9774fc1bd6330014aada32ecc82ca4e81a0
JanuaryClouds <janmichaelcristianbcruz@gmail.com>
2026-04-24 01:17:47 +0800

    fix | Zero | handle client fetching in the backend

--- app/Http/Controllers/CitizenAccessController.php
+++ app/Http/Controllers/CitizenAccessController.php
@@ -5,10 +5,12 @@ namespace App\Http\Controllers;
 use App\Models\ApplicationStep;
 use App\Models\DocumentRequirement;
 use App\Models\SystemSetting;
+use App\Models\User;
 use App\Services\CentralClientService;
 use App\Services\ImageService;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Str;
 
 class CitizenAccessController extends Controller
 {
@@ -35,8 +37,46 @@ class CitizenAccessController extends Controller
         $burialDocuments = DocumentRequirement::burial();
         $funeralDocuments = DocumentRequirement::funeral();
 
-        if (app()->isLocal()) {
-            return view('test.zero');
+        if (config('services.portal.mock')) {
+            $citizens = User::whereNotNull('citizen_uuid')->get();
+            $testLinks = [];
+            foreach ($citizens as $citizen) {
+                $payload = [
+                    'citizen_uuid' => $citizen->citizen_uuid,
+                    'time' => time(),
+                    'nonce' => Str::random(32),
+                ];
+
+                $encoded = rtrim(strtr(base64_encode(json_encode($payload)), '+/', '-_'), '=');
+                $signature = hash_hmac('sha256', $encoded, config('services.portal.sso.secret'));
+
+                $url = url('/sso/callback')."?payload={$encoded}&signature={$signature}";
+
+                $testLinks[] = [
+                    'name' => $citizen->fullname(),
+                    'url' => $url,
+                ];
+            }
+
+            $newUser = [
+                'citizen_uuid' => Str::uuid()->toString(),
+                'time' => time(),
+                'nonce' => Str::uuid()->toString(),
+            ];
+
+            $encoded = rtrim(strtr(base64_encode(json_encode($newUser)), '+/', '-_'), '=');
+            $signature = hash_hmac('sha256', $encoded, config('services.portal.sso.secret'));
+
+            $url = url('/sso/callback')."?payload={$encoded}&signature={$signature}";
+
+            $testLinks[] = [
+                'name' => 'New User',
+                'url' => $url,
+            ];
+
+            return view('test.zero', [
+                'testUsers' => $testLinks,
+            ]);
         }
 
         return view('landing', compact(
@@ -49,6 +89,7 @@ class CitizenAccessController extends Controller
 
     public function sso()
     {
+        // example URL sent from portal: https://funeral.taguig.gov.ph/sso/callback?payload={$payload}&signature={$signature}
         $ssoRequest = request('payload');
         $signature = request('signature');
 
@@ -93,13 +134,34 @@ class CitizenAccessController extends Controller
                 session(['api_token' => $token]);
             }
 
-            if (Auth::user()->clients()->count() == 0) {
-                return redirect()->route('general.intake.form');
-            } else {
-                return redirect()->route('dashboard');
-            }
+            $redirect = auth()->user()->clients()->exists() ? route('dashboard') : route('general.intake.form');
+
+            return redirect()->to($redirect);
         } else {
             abort(403);
         }
+    }
+
+    public function logout()
+    {
+        $payload = [
+            'citizen_uuid' => auth()->user()->citizen_uuid,
+            'time' => time(),
+            'nonce' => Str::random(32),
+        ];
+
+        $encoded = rtrim(strtr(base64_encode(json_encode($payload)), '+/', '-_'), '=');
+        $signature = hash_hmac('sha256', $encoded, config('services.portal.sso.secret'));
+
+        $user = Auth::user();
+        $user->tokens()->delete();
+        Auth::logout();
+        request()->session()->invalidate();
+        request()->session()->regenerateToken();
+        if (session()->has('citizen')) {
+            session()->forget('citizen');
+        }
+
+        return redirect('/');
     }
 }
--- resources/views/test/zero.blade.php
+++ resources/views/test/zero.blade.php
@@ -17,28 +17,23 @@
                         TLC
                         Portal link to be a form containing hidden inputs for a key and the citizen's uuid</p>
                     <div class="row g-1">
-                        @php
-                            $citizens = App\Models\User::whereNotNull('citizen_uuid')
-                                ->inRandomOrder()
-                                ->limit(10)
-                                ->get();
-                        @endphp
-                        @foreach ($citizens as $citizen)
+                        @foreach ($testUsers as $user)
                             @php
-                                $payload = [
-                                    'citizen_uuid' => $citizen->citizen_uuid,
-                                    'time' => time(),
-                                    'nonce' => Str::uuid()->toString(),
-                                ];
-
-                                $encoded = rtrim(strtr(base64_encode(json_encode($payload)), '+/', '-_'), '=');
-                                $signature = hash_hmac('sha256', $encoded, config('services.portal.sso.secret'));
-
-                                $url = url('/sso/callback') . "?payload={$encoded}&signature={$signature}";
+                                // DONE: handle in backend instead of blade template
+                                // DONE: send to group chat as reference, as well as the sso and endpoint
+                                // DONE: add a non-existing user link for testing
+                                // DONE: use development version of production from hostinger
+                                // DONE: use the TLC portal notification API endpoint when creating notifications
+                                // DONE: update the API endpoint of TLC portal for user info fetching
+                                // DONE: create or edit an endpoint that allows the portal to check the session of this system
+                                // DONE: when citizen accesses the system and clicks the funeral assistance card, send an API call or expose an endpoint telling the portal that the citizen has a session in funeral assistance system
+                                // DONE: when the citizen logs out, send an API call or expose an endpoint telling the portal that the citizen has logged out of funeral assistance system AND the portal
+                                // DONE: add a hashed signature to the logout endpoint, same as the SSO process
+                                // DONE: notification panel might not be necessary as notifications are handled by the portal
                             @endphp
                             <div class="col-4 d-flex">
-                                <a href="{{ $url }}" class="btn btn-secondary w-100">
-                                    {{ $citizen->citizen_uuid }}
+                                <a href="{{ $user['url'] }}" class="btn btn-secondary w-100">
+                                    {{ $user['name'] }}
                                 </a>
                             </div>
                         @endforeach
