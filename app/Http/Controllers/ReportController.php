5cbd2c7d9b307d8eb99e07e6c522a49a66b04085
JanuaryClouds <janmichaelcristianbcruz@gmail.com>
2026-05-12 15:58:59 +0800

    fix | Report Controller | cleaned up compact

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -106,7 +106,13 @@ class ReportController extends Controller
             ]);
         }
 
-        return view('reports.index', compact('data', 'columns', 'model', 'startDate', 'endDate'));
+        return view('reports.index', compact(
+            'data', 
+            'columns', 
+            'model',
+            'startDate', 
+            'endDate'
+        ));
     }
 
     public function burialAssistances(Request $request)
@@ -136,55 +142,6 @@ class ReportController extends Controller
         $deceasedPerBarangay = $this->reportServices->deceasedPerBarangay($startDate, $endDate);
         $deceasedPerReligion = $this->reportServices->deceasedPerReligion($startDate, $endDate);
 
-        $cardData = [
-            [
-                'label' => 'All',
-                'icon' => 'ki-file',
-                'pathsCount' => 2,
-                'count' => count($data->toArray()),
-            ],
-            [
-                'label' => 'Pending',
-                'icon' => 'ki-file',
-                'pathsCount' => 2,
-                'count' => count(array_filter($data->toArray(), function ($item) {
-                    return $item['status'] == 'Pending';
-                })),
-            ],
-            [
-                'label' => 'Processing',
-                'icon' => 'ki-file-right',
-                'pathsCount' => 2,
-                'count' => count(array_filter($data->toArray(), function ($item) {
-                    return $item['status'] == 'Processing';
-                })),
-            ],
-            [
-                'label' => 'For Pickup',
-                'icon' => 'ki-file-added',
-                'pathsCount' => 2,
-                'count' => count(array_filter($data->toArray(), function ($item) {
-                    return $item['status'] == 'For Pickup';
-                })),
-            ],
-            [
-                'label' => 'Released',
-                'icon' => 'ki-folder-added',
-                'pathsCount' => 2,
-                'count' => count(array_filter($data->toArray(), function ($item) {
-                    return $item['status'] == 'Released';
-                })),
-            ],
-            [
-                'label' => 'Rejected',
-                'icon' => 'ki-delete-folder',
-                'pathsCount' => 2,
-                'count' => count(array_filter($data->toArray(), function ($item) {
-                    return $item['status'] == 'Rejected';
-                })),
-            ],
-        ];
-
         return view('reports.index', compact(
             'data',
             'columns',
@@ -193,7 +150,6 @@ class ReportController extends Controller
             'deceasedPerReligion',
             'startDate',
             'endDate',
-            'cardData',
         ));
     }
 
