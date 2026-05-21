3caa36ac9fe6677f2ca80081bc2361845bd37178
JanuaryClouds <janmichaelcristianbcruz@gmail.com>
2026-05-21 12:23:25 +0800

    fix | Client Info | only wrap button when editing info

--- resources/views/client/view.blade.php
+++ resources/views/client/view.blade.php
@@ -9,28 +9,26 @@
             @include('client.partials.referral-modal')
         @endif
         @include('client.partials.interview-alert')
-        @role('superadmin')
-            <form action="{{ route('client.update', $client->id) }}" method="post">
-                @csrf
-                @method('put')
-                <div class="card">
-                    <div class="card-body">
-                        @include('client.partials.client-info', [
-                            'client' => $client,
-                            'readonly' => $readonly,
-                        ])
-                    </div>
+        <form action="{{ route('client.update', $client->id) }}" method="post">
+            @csrf
+            @method('put')
+            <div class="card">
+                <div class="card-body">
+                    @include('client.partials.client-info', [
+                        'client' => $client,
+                        'readonly' => $readonly,
+                    ])
+                </div>
+                @role('superadmin')
                     <div class="card-footer d-flex justify-content-end">
                         <button class="btn btn-warning btn-sm" id="saveContentBtn">
                             <i class="fas fa-save"></i>
                             Save Changes to Data
                         </button>
                     </div>
-                </div>
-            </form>
-        @else
-            @include('client.partials.client-info')
-        @endrole
+                @endrole
+            </div>
+        </form>
         <div class="card">
             <div class="card-body">
                 @include('client.partials.beneficiary-info', [
@@ -38,10 +36,12 @@
                     'readonly' => true,
                 ])
             </div>
-            <div class="card-footer d-flex justify-content-end">
-                <a name="" id="" class="btn btn-warning btn-sm"
-                    href="{{ route('beneficiary.show', $client->beneficiary->id) }}" role="button">Edit Data</a>
-            </div>
+            @role('superadmin')
+                <div class="card-footer d-flex justify-content-end">
+                    <a name="" id="" class="btn btn-warning btn-sm"
+                        href="{{ route('beneficiary.show', $client->beneficiary->id) }}" role="button">Edit Data</a>
+                </div>
+            @endrole
         </div>
         <div class="card">
             <div class="card-body d-flex flex-column gap-2">
