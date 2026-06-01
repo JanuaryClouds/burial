3ca99f2d0786f6442cedb6010d66c5cd6b009469
JanuaryClouds <janmichaelcristianbcruz@gmail.com>
2026-06-01 12:29:03 +0800

    fix | Settings Index, Role Index | improved UI

--- resources/views/cms/partials/edit-role.blade.php
+++ resources/views/cms/partials/edit-role.blade.php
@@ -1,9 +1,14 @@
-@foreach ($permissions as $permission)
-    <div>
-        <label>
-            <input type="checkbox" name="permissions[]" value="{{ $permission->name }}"
-                {{ in_array($permission->name, $data->permissions->pluck('name')->toArray()) ? 'checked' : '' }}>
-            {{ Str::title(str_replace(['_', '-'], ' ', $permission->name)) }}
-        </label>
+<div class="row">
+    <div class="col d-flex flex-column gap-2">
+        @foreach ($permissions as $permission)
+            <div class="form-check">
+                <input class="form-check-input" type="checkbox" name="permissions[]" value="{{ $permission->name }}"
+                    id="permission{{ $permission->id }}Check"
+                    {{ in_array($permission->name, $data->permissions->pluck('name')->toArray()) ? 'checked' : '' }}>
+                <label class="form-check-label" for="permission{{ $permission->id }}Check">
+                    {{ Str::title(str_replace(['_', '-'], ' ', $permission->name)) }}
+                </label>
+            </div>
+        @endforeach
     </div>
-@endforeach
+</div>
