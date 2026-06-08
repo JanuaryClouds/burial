414281c8bc5ffc3c97ef956433a37d2069978f84
JanuaryClouds <janmichaelcristianbcruz@gmail.com>
2026-06-08 08:29:15 +0800

    WIP | Larastan

--- app/Models/Beneficiary.php
+++ app/Models/Beneficiary.php
@@ -5,7 +5,8 @@ namespace App\Models;
 use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Str;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Support\Str;
 
 class Beneficiary extends Model
 {
@@ -41,12 +42,20 @@ class Beneficiary extends Model
         'suffix' => 'encrypted',
     ];
 
-    public function tracking_no()
+    /**
+     * Summary of tracking_no
+     * @return string returns the tracking number of the client this beneficiary is created from
+     */
+    public function tracking_no(): string
     {
         return $this->client?->tracking_no;
     }
 
-    public function fullname()
+    /**
+     * Summary of fullname
+     * @return string returns the full name of the beneficiary, with the middlename being shortened
+     */
+    public function fullname(): string
     {
         return $this->first_name.' '.
             ($this->middle_name ? Str::substr($this->middle_name, 0, 1).'. ' : '').
@@ -54,12 +63,20 @@ class Beneficiary extends Model
             ($this->suffix ? ' '.$this->suffix : '');
     }
 
-    public function age()
+    /**
+     * Summary of age
+     * @return int returns the age of the beneficiary 
+     */
+    public function age(): int
     {
         return Carbon::parse($this->created_at)->diffInYears($this->date_of_birth);
     }
 
-    public function assistance()
+    /**
+     * Summary of assistance
+     * @return mixed returns the assistance of the beneficiary, could be burial or funeral
+     */
+    public function assistance(): mixed
     {
         if ($this->client?->claimant?->count() > 0) {
             return $this->client?->claimant?->burialAssistance;
@@ -72,27 +89,38 @@ class Beneficiary extends Model
         return null;
     }
 
-    public static function getBeneficiary($client)
-    {
-        return self::where('client_id', $client)->first();
-    }
-
-    public function sex()
+    /**
+     * Summary of sex
+     * @return BelongsTo<Sex>
+     */
+    public function sex(): BelongsTo
     {
         return $this->belongsTo(Sex::class, 'sex_id');
     }
 
-    public function client()
+    /**
+     * Summary of client
+     * @return BelongsTo<Client>
+     */
+    public function client(): BelongsTo
     {
         return $this->belongsTo(Client::class, 'client_id');
     }
 
-    public function religion()
+    /**
+     * Summary of religion
+     * @return BelongsTo<Religion>
+     */
+    public function religion(): BelongsTo
     {
         return $this->belongsTo(Religion::class, 'religion_id');
     }
 
-    public function barangay()
+    /**
+     * Summary of barangay
+     * @return BelongsTo<Barangay>
+     */
+    public function barangay(): BelongsTo
     {
         return $this->belongsTo(Barangay::class, 'barangay_id');
     }
