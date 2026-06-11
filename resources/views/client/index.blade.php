f70dc3af3aa2fd63efc44a8c0f09aa60ad19ed9b
JanuaryClouds <janmichaelcristianbcruz@gmail.com>
2026-06-11 09:55:30 +0800

    fix | Cards Grid | added vertial spacing

--- resources/views/client/index.blade.php
+++ resources/views/client/index.blade.php
@@ -3,7 +3,7 @@
     <div class="d-flex flex-column gap-4">
         @role('staff')
             @if (isset($cardData) && is_array($cardData) && count($cardData) > 0)
-                <div class="row">
+                <div class="row gy-4">
                     @foreach ($cardData as $card)
                         <div class="col-6 col-lg-3">
                             <livewire:counter :model="$card['model']" :label="$card['label']" :scope="$card['scope']" :iconName="$card['iconName']"
