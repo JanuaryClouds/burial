99e15dfbc9d960c7f7531823e0d18f41bc5c2754
JanuaryClouds <janmichaelcristianbcruz@gmail.com>
2026-07-10 23:47:42 +0800

    WIP | Recommendations

---
+++ database/migrations/data/2026_07_10_170952_create_recommendations_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('recommendations', function (Blueprint $table) {
+            $table->uuid()->primary();
+            $table->foreignUuid('application_uuid')
+                ->constrained('applications', 'uuid')
+                ->cascadeOnDelete();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('recommendations');
+    }
+};
