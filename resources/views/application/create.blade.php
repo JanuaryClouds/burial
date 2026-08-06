29f970dd2fdb35f2b137a329cea269f790915866
JanuaryClouds <janmichaelcristianbcruz@gmail.com>
2026-08-06 21:45:02 +0800

    feat | Application Wizard | added images

--- resources/views/application/create.blade.php
+++ resources/views/application/create.blade.php
@@ -2,7 +2,9 @@
 @section('content')
 	<form action="{{ route('application.store') }}"
 		method="POST"
-		id="applicationForm">
+		class="d-flex flex-column gap-8"
+		id="applicationForm"
+		enctype="multipart/form-data">
 		@csrf
 		<div class="row gap-y-2 gap-lg-0">
 			<div class="col-12 col-md-12 col-lg-6">
@@ -15,6 +17,7 @@
 						:selected="session('client_uuid') ?? null"
 						:options="$clientOptions ?? []" />
 					<x-form.select name="relationship_id"
+						id="relationship_select"
 						label="Relationship to Beneficiary"
 						:required="true"
 						:options="$relationships ?? []" />
@@ -32,6 +35,18 @@
 				</x-card>
 			</div>
 		</div>
+
+		<x-card>
+			<x-slot:header>
+				Submitted Documents
+			</x-slot:header>
+			<div class="alert alert-warning mb-3"
+				role="alert">
+				<i class="fa-solid fa-info-circle me-2"></i><strong>Note:</strong>
+				During the interview, please bring hard copies of the documents you have submitted as soft copies.
+			</div>
+			@include('application.partials.documents')
+		</x-card>
 	</form>
 
 	<livewire:client.display :draftClients="$draftClients ?? []"
@@ -39,35 +54,81 @@
 	<livewire:beneficiary.display :draftBeneficiaries="$draftBeneficiaries ?? []"
 		:uuid="session('beneficiary_uuid') ?? null" />
 
-	{{-- TODO add images --}}
-
 	<x-card>
 		<div class="d-flex justify-content-end align-items-center gap-2">
-			<x-modal modalId="confirmModal"
-				buttonClass="btn btn-success btn-sm"
-				modalTitle="Confirm Application Submission"
-				modalSize="lg">
-				<x-slot:triggerButton>
-					<i class="fa-solid fa-check"></i>
-					Submit Application
-				</x-slot:triggerButton>
-				<p class="fs-4">Are you sure you want to submit all of these information into your application. Once submitted,
-					editing will be disabled</p>
-				<x-slot:footer>
-					<button type="button"
-						class="btn btn-secondary"
-						data-bs-dismiss="modal">
-						<i class="fa-solid fa-xmark"></i>
-						Cancel
-					</button>
-					<button type="submit"
-						form="applicationForm"
-						class="btn btn-success">
-						<i class="fa-solid fa-check"></i>
-						Submit
-					</button>
-				</x-slot:footer>
-			</x-modal>
+			<button type="button"
+				class="btn btn-sm btn-light"
+				id="disabledSaveButton"
+				data-bs-toggle="tooltip"
+				data-bs-placement="top"
+				title="Complete the application form first">
+				<i class="fa-solid fa-floppy-disk"></i>
+				Submit Application
+			</button>
+
+			<div id="saveButton">
+				<x-modal modalId="confirmModal"
+					buttonClass="btn btn-success btn-sm"
+					modalTitle="Confirm Application Submission"
+					modalSize="lg">
+					<x-slot:triggerButton>
+						<i class="fa-solid fa-floppy-disk"></i>
+						Submit Application
+					</x-slot:triggerButton>
+					<p class="fs-4">Are you sure you want to submit all of these information into your application. Once submitted,
+						editing will be disabled</p>
+					<x-slot:footer>
+						<button type="button"
+							class="btn btn-secondary"
+							data-bs-dismiss="modal">
+							<i class="fa-solid fa-xmark"></i>
+							Cancel
+						</button>
+						<button type="submit"
+							form="applicationForm"
+							class="btn btn-success">
+							<i class="fa-solid fa-check"></i>
+							Submit
+						</button>
+					</x-slot:footer>
+				</x-modal>
+			</div>
 		</div>
 	</x-card>
+	<script nonce="{{ $nonce ?? '' }}">
+		$(document).ready(function() {
+			$('#saveButton').hide();
+			$('#disabledSaveButton').show();
+			let client_uuid = $('#client_uuid_select').find('option:selected').val().trim();
+			let beneficiary_uuid = $('#beneficiary_uuid_select').find('option:selected').val().trim();
+			let relationship_id = $('#relationship_select').find('option:selected').val().trim();
+
+			$('#client_uuid_select').on('change', function() {
+				client_uuid = $(this).find('option:selected').val().trim();
+				validateForm();
+			});
+
+			$('#beneficiary_uuid_select').on('change', function() {
+				beneficiary_uuid = $(this).find('option:selected').val().trim();
+				validateForm();
+			});
+
+			$('#relationship_select').on('change', function() {
+				relationship_id = $(this).find('option:selected').val().trim();
+				validateForm();
+			});
+
+			function validateForm() {
+				if (client_uuid !== '' && beneficiary_uuid !== '' && relationship_id !== '') {
+					$('#saveButton').show();
+					$('#disabledSaveButton').hide();
+				} else {
+					$('#saveButton').hide();
+					$('#disabledSaveButton').show();
+				}
+			};
+
+			validateForm();
+		});
+	</script>
 @endsection
