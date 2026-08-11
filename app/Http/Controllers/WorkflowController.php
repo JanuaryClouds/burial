e007b4c398f155a2b2385fb095d992650d1daf63
JanuaryClouds <janmichaelcristianbcruz@gmail.com>
2026-08-11 15:54:56 +0800

    feat | Workflow Controller | prepared

--- app/Http/Controllers/WorkflowController.php
+++ app/Http/Controllers/WorkflowController.php
@@ -5,15 +5,33 @@ namespace App\Http\Controllers;
 use App\Models\Workflow;
 use App\Http\Requests\StoreWorkflowRequest;
 use App\Http\Requests\UpdateWorkflowRequest;
+use App\Services\DatatableService;
+use App\Services\WorkflowService;
+use Illuminate\Support\Facades\Auth;
 
 class WorkflowController extends Controller
 {
+    public function __construct(
+        private readonly WorkflowService $services,
+        private readonly DatatableService $datatableServices,
+    ) {}
+
     /**
      * Display a listing of the resource.
      */
     public function index()
     {
-        //
+        $workflows = $this->services->index();
+
+        if (request()->expectsJson()) {
+            return $this->datatableServices->ajax($workflows);
+        }
+
+        return view('workflow.index', [
+            'workflows' => $workflows,
+            'page_title' => 'Workflows',
+            'columns' => $this->datatableServices->getColumns($workflows),
+        ]);
     }
 
     /**
@@ -21,7 +39,9 @@ class WorkflowController extends Controller
      */
     public function create()
     {
-        //
+        return view('workflow.create', [
+            'page_title' => 'Create Workflow',
+        ]);
     }
 
     /**
@@ -29,7 +49,19 @@ class WorkflowController extends Controller
      */
     public function store(StoreWorkflowRequest $request)
     {
-        //
+        try {
+            $workflow = $this->services->store($request->validated());
+            activity()
+                ->withProperties(['ip' => request()->ip(), 'browser' => request()->userAgent(), 'workflow' => $workflow->uuid])
+                ->causedBy(Auth::user())
+                ->log('Created a new workflow: '.$workflow->uuid);
+
+            return redirect()
+                ->route('workflow.show', $workflow)
+                ->with('success', 'Created successfully');
+        } catch (\Exception $e) {
+            return redirect()->back()->with('error', app()->hasDebugModeEnabled() ? $e->getMessage() : 'An error occurred. Please try again.');
+        }
     }
 
     /**
@@ -37,7 +69,10 @@ class WorkflowController extends Controller
      */
     public function show(Workflow $workflow)
     {
-        //
+        return view('workflow.show', [
+            'workflow' => $workflow,
+            'page_title' => $workflow->name,
+        ]);
     }
 
     /**
@@ -45,7 +80,10 @@ class WorkflowController extends Controller
      */
     public function edit(Workflow $workflow)
     {
-        //
+        return view('workflow.edit', [
+            'workflow' => $workflow,
+            'page_title' => $workflow->name,
+        ]);
     }
 
     /**
@@ -53,7 +91,18 @@ class WorkflowController extends Controller
      */
     public function update(UpdateWorkflowRequest $request, Workflow $workflow)
     {
-        //
+        try {
+            $this->services->update($request->validated(), $workflow);
+            activity()
+                ->withProperties(['ip' => request()->ip(), 'browser' => request()->userAgent(), 'workflow' => $workflow->uuid])
+                ->causedBy(Auth::user())
+                ->log('Updated the workflow details: '.$workflow->uuid);
+            return redirect()
+                ->route('workflow.show', $workflow)
+                ->with('success', 'Updated successfully');
+        } catch (\Exception $e) {
+            return redirect()->back()->with('error', app()->hasDebugModeEnabled() ? $e->getMessage() : 'Unable to update workflow. Please try again.');
+        }
     }
 
     /**
