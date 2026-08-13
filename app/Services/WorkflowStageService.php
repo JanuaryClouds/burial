3ac0ba64a39c51830172876239fed79fcb9dabc4
JanuaryClouds <janmichaelcristianbcruz@gmail.com>
2026-08-13 13:09:07 +0800

    feat | Workflow Stage Service | store function

--- app/Services/WorkflowStageService.php
+++ app/Services/WorkflowStageService.php
@@ -2,10 +2,21 @@
 
 namespace App\Services;
 
+use App\Models\WorkflowStage;
+use Illuminate\Support\Facades\DB;
+use Spatie\Permission\Models\Permission;
+
 class WorkflowStageService
 {
     public function __construct()
     {
         //
+    }
+
+    public function store(array $data): WorkflowStage
+    {   
+        return DB::transaction(function () use ($data) {
+            return WorkflowStage::create($data);
+        });
     }
 }
