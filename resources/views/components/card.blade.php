506d5ac9dcd10662cf8335e64c3b096c51c55e28
JanuaryClouds <janmichaelcristianbcruz@gmail.com>
2026-08-14 21:16:24 +0800

    fix | Card | merge attributes

--- resources/views/components/card.blade.php
+++ resources/views/components/card.blade.php
@@ -1,4 +1,4 @@
-<div class="card multicolor-border">
+<div {{ $attributes->merge(['class' => 'card multicolor-border']) }}>
 	@isset($header)
 		<div class="card-header">
 			<h5 class="card-title">{{ $header }}</h5>
