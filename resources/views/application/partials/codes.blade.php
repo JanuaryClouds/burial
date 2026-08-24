11c0f6bd03471675239f94383910c6eb027949c4
JanuaryClouds <janmichaelcristianbcruz@gmail.com>
2026-08-24 15:56:16 +0800

    chore | Codes | removed card wrapper

--- resources/views/application/partials/codes.blade.php
+++ resources/views/application/partials/codes.blade.php
@@ -1,21 +1,9 @@
 @props(['application', 'qrCode', 'barcode'])
-<x-card>
-	<x-slot:header>Tracking Codes</x-slot:header>
-	<div class="d-flex flex-column flex-center">
-		<img src="{{ $qrCode }}"
-			alt="{{ $application->qr_code }}"
-			style="width: 200px; height: 200px;" />
-		<img src="{{ $barcode }}"
-			alt="{{ $application->qr_code }}"
-			style="width: 300px; height: 100px;" />
-	</div>
-	<x-slot:footer>
-		<a class="btn btn-sm btn-primary"
-			target="_blank"
-			href="{{ route('application.codes', $application) }}"
-			role="button">
-			<i class="fa-solid fa-print"></i>
-			Print
-		</a>
-	</x-slot:footer>
-</x-card>
+<div class="d-flex flex-column flex-center">
+	<img src="{{ $qrCode }}"
+		alt="{{ $application->qr_code }}"
+		style="width: 200px; height: 200px;" />
+	<img src="{{ $barcode }}"
+		alt="{{ $application->qr_code }}"
+		style="width: 300px; height: 100px;" />
+</div>
