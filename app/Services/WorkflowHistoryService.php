4a9bfc807853a31a22f11710fd46958a1cbbd928
JanuaryClouds <janmichaelcristianbcruz@gmail.com>
2026-08-25 15:57:07 +0800

    feat | WorkflowHistory Service | store function

--- app/Services/WorkflowHistoryService.php
+++ app/Services/WorkflowHistoryService.php
@@ -2,10 +2,17 @@
 
 namespace App\Services;
 
+use App\Models\WorkflowHistory;
+
 class WorkflowHistoryService
 {
     public function __construct()
     {
         //
+    }
+
+    public function store(array $data): WorkflowHistory
+    {
+        return WorkflowHistory::create($data);
     }
 }
