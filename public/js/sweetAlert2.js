d37b02d7bf2104eaa8ddd9ade3888222e4c4f15e
JanuaryClouds <janmichaelcristianbcruz@gmail.com>
2026-09-07 20:41:14 +0800

    feat | SweetAlert | listen to livewire events

---
+++ public/js/sweetAlert2.js
@@ -0,0 +1,15 @@
+export default function sweetAlert() {
+    Livewire.on('notification:alert', (event) => {
+        const data = event[0];
+
+        console.log(data);
+
+        Swal.fire({
+            icon: data.type,
+            title: data.title,
+            text: data.text,
+			timerProgressBar: true,
+			buttonsStyling: true,
+        })
+    });
+};
