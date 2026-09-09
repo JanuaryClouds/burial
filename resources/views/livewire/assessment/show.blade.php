6a59631db5e8d7ee05c048f09ad73e53f6223adc
JanuaryClouds <janmichaelcristianbcruz@gmail.com>
2026-09-09 12:47:26 +0800

    fix | Show Assessment | increased padding of fields

--- resources/views/livewire/assessment/show.blade.php
+++ resources/views/livewire/assessment/show.blade.php
@@ -1,11 +1,11 @@
 <div class="d-flex flex-column gap-2">
 	<strong>Problem Presented:</strong>
-	<div class="bg-gray-200 rounded px-2 py-1">
+	<div class="bg-gray-200 rounded px-3 py-2">
 		{{ $assessment->problem_presented }}
 	</div>
 	<div class="separator separator-dotted my-4"></div>
 	<strong>Social Worker's Assessment:</strong>
-	<div class="bg-gray-200 rounded px-2 py-1">
+	<div class="bg-gray-200 rounded px-3 py-2">
 		{{ $assessment->swa }}
 	</div>
 </div>
