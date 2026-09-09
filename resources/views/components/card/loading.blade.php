d1f933966d7ec2f2eaa0b473a0f205b1d9ad6544
JanuaryClouds <janmichaelcristianbcruz@gmail.com>
2026-09-09 12:43:54 +0800

    chore | Loading Card | removed card slots

--- resources/views/components/card/loading.blade.php
+++ resources/views/components/card/loading.blade.php
@@ -1,11 +1,3 @@
-<x-card>
-	@isset($header)
-		<x-slot:header>{{ $header }}</x-slot:header>
-	@endisset
-	<div class="d-flex flex-center w-full h-250px">
-		<x-icon.font-awesome class="spinner spinner-border fs-4" />
-	</div>
-	@isset($footer)
-		<x-slot:footer>{{ $footer }}</x-slot:footer>
-	@endisset
-</x-card>
+<div class="d-flex flex-center w-full h-250px">
+	<x-icon.font-awesome class="spinner spinner-border fs-4" />
+</div>
