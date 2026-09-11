a0fd0d1f12ab7bf029febdf620ef34dc885b4f59
JanuaryClouds <janmichaelcristianbcruz@gmail.com>
2026-09-11 10:25:59 +0800

    fix | Create Client | added optional fields in info card

--- resources/views/livewire/client/create.blade.php
+++ resources/views/livewire/client/create.blade.php
@@ -12,7 +12,8 @@
 						<x-slot:title>
 							<p class="text-danger fs-6 fw-bold">This from has required fields</p>
 						</x-slot:title>
-						<p class="text-danger">All fields marked with an asterisk (*) are required to be filled out.</p>
+						<p class="text-danger">All fields marked with an asterisk (*) are required to be filled out. Leave blank if not
+							required and inapplicable.</p>
 					</x-callout>
 				</div>
 				<div class="col-12 col-md-6 d-flex flex-grow">
