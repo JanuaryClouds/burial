259f5a140f653396d0481fa5ed20e76b2e8d205d
JanuaryClouds <janmichaelcristianbcruz@gmail.com>
2026-09-11 10:23:23 +0800

    fix | Callout | set width to max

--- resources/views/components/callout.blade.php
+++ resources/views/components/callout.blade.php
@@ -1,4 +1,4 @@
-<div {{ $attributes->merge(['class' => 'd-flex justify-content-start gap-2 py-4 px-5 border-start border-5']) }}>
+<div {{ $attributes->merge(['class' => 'd-flex justify-content-start w-100 gap-2 py-4 px-5 border-start border-5']) }}>
 	@isset($icon)
 		<div>
 			{{ $icon }}
