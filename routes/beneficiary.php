3724f8baabf39ecae124cb72d29b8869dadc3114
JanuaryClouds <janmichaelcristianbcruz@gmail.com>
2026-09-18 15:48:59 +0800

    feat | added Show and Edit route and views for Beneficiary Family

--- routes/beneficiary.php
+++ routes/beneficiary.php
@@ -21,36 +21,24 @@ Route::name('beneficiary.')
         Route::prefix('/{beneficiary}')
             ->group(function () {
                 Route::get('', 'show')
-                    ->middleware('can:view,beneficiary')
+                    ->middleware('can:view,\App\Models\Beneficiary,beneficiary')
                     ->name('show');
 
                 Route::get('/edit', 'edit')
-                    ->middleware('can:update,beneficiary')
+                    ->middleware('can:update,\App\Models\Beneficiary,beneficiary')
                     ->name('edit');
             });
+
+            Route::name('family.')
+                ->controller(BeneficiaryFamilyController::class)
+                ->prefix('family/')
+                ->group(function () {
+                    Route::prefix('{member}')
+                        ->group(function () {
+                            Route::get('/edit', 'edit')
+                                ->middleware('can:update,\App\Models\BeneficiaryFamily,member')
+                                ->name('edit');
+                        });
+                });
     });
 
-Route::name('family.')
-    ->controller(BeneficiaryFamilyController::class)
-    ->prefix('family/')
-    ->group(function () {
-        Route::get('', 'create')
-            ->name('create');
-
-        Route::post('/store', 'store')
-            ->name('store');
-
-        Route::prefix('{member}')
-            ->group(function () {
-                Route::get('', 'show')
-                    ->name('show');
-
-                Route::get('/edit', 'edit')
-                    ->middleware('can:update,member')
-                    ->name('edit');
-
-                Route::post('/update', 'update')
-                    ->middleware('can:update,member')
-                    ->name('update');
-            });
-    });
