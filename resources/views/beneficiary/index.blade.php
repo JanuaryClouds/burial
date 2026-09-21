9f1756f6dbe67fd2bef58d95a9396fb51b699335
JanuaryClouds <janmichaelcristianbcruz@gmail.com>
2026-09-21 15:31:41 +0800

    feat | Beneficiary Index | added charts and statistics

--- resources/views/beneficiary/index.blade.php
+++ resources/views/beneficiary/index.blade.php
@@ -15,4 +15,29 @@
 			</x-slot:footer>
 		@endunlessrole
 	</x-card>
+	<div class="row">
+		<div class="col-12 col-lg-6 col-xl-4">
+			<x-card>
+				<x-slot:header>Beneficiaries Per Age Group</x-slot:header>
+				<livewire:beneficiary.charts.age-group />
+			</x-card>
+		</div>
+		<div class="col-12 col-lg-6 col-xl-4">
+			<x-card>
+				<x-slot:header>Perinatal and Neonatal Deaths</x-slot:header>
+				<livewire:beneficiary.charts.natality-group />
+			</x-card>
+		</div>
+		<div class="col-12 col-lg-6 col-xl-4">
+			<x-card>
+				<x-slot:header>Beneficiaries Per Religion</x-slot:header>
+				<livewire:beneficiary.charts.religion-group />
+			</x-card>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-6 col-lg-3">
+			<livewire:beneficiary.charts.pwd-count />
+		</div>
+	</div>
 @endsection
