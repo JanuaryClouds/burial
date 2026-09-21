5aa8274aa1087ea2219d2f18dff5595b338bc1d5
JanuaryClouds <janmichaelcristianbcruz@gmail.com>
2026-09-21 15:30:00 +0800

    chore | Counter | simplified

--- app/Livewire/Counter.php
+++ app/Livewire/Counter.php
@@ -8,63 +8,26 @@ use Livewire\Component;
 
 class Counter extends Component
 {
-    #[Locked]
-    public string $model;
+    public string $label;
 
-    #[Locked]
-    public ?string $scope = null;
-
-    #[Locked]
-    public ?string $label = null;
-
-    #[Locked]
-    public ?string $iconName = null;
-
-    #[Locked]
-    public ?int $iconPathsCount = null;
-
-    #[Locked]
     public int $count = 0;
 
-    #[Locked]
-    public ?string $route = null;
+    public string $iconName;
 
-    protected $listeners = [
-        'refreshCounter' => 'loadCount',
-    ];
+    public int $iconPathsCount;
+
+    public string $route;
 
     public function mount(
-        string $model,
-        ?string $scope = null,
-        ?string $label = null,
-        ?string $iconName = null,
-        ?int $iconPathsCount = null,
-        ?string $route = null
+        string $label,
+        string $iconName,
+        int $iconPathsCount,
+        string $route
     ) {
-        $this->model = $model;
-        $this->scope = $scope;
         $this->label = $label;
         $this->iconName = $iconName;
         $this->iconPathsCount = $iconPathsCount;
         $this->route = $route;
-
-        $this->loadCount();
-    }
-
-    public function loadCount()
-    {
-        if (! class_exists($this->model) || ! is_subclass_of($this->model, Model::class)) {
-            $this->count = 0;
-
-            return;
-        }
-
-        $query = ($this->model)::query();
-        if ($this->scope && method_exists($this->model, 'scope'.ucfirst($this->scope))) {
-            $query->{$this->scope}();
-        }
-
-        $this->count = $query->count();
     }
 
     public function render()
