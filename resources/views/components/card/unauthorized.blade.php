c8997d03eefb489c09a45e3de389ac4e88776991
JanuaryClouds <janmichaelcristianbcruz@gmail.com>
2026-09-23 15:44:51 +0800

    feat | Unauthorized Card | added default warning message

--- resources/views/components/card/unauthorized.blade.php
+++ resources/views/components/card/unauthorized.blade.php
@@ -3,7 +3,11 @@
 		<x-slot:header>{{ $header }}</x-slot:header>
 	@endisset
 	<div class="d-flex flex-center flex-column gap-4 text-gray-400">
-		{{ $slot }}
+		@isset($slot)
+			{{ $slot }}
+		@else
+			You do not have permission to view this section.
+		@endisset
 	</div>
 	<x-slot:footer>
 		<small class="text-muted">
