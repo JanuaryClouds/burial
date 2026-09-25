33a1492306c17d22f845a2a2b5886da76cfbd6b4
JanuaryClouds <janmichaelcristianbcruz@gmail.com>
2026-09-25 15:46:38 +0800

    WIP | Report Page | Beneficiary Summary

---
+++ resources/views/report/index/partials/beneficiary/summary.blade.php
@@ -0,0 +1,13 @@
+<div class="d-flex flex-column gap-6">
+	<div class="row">
+		{{-- start::Counts Table --}}
+		{{-- end::Counts Table --}}
+
+		{{-- start::Per Age Group Chart --}}
+		{{-- end::Per Age Group Chart --}}
+	</div>
+	<div class="row">
+		{{-- start::Per Religion Chart --}}
+		{{-- end::Per Religion Chart --}}
+	</div>
+</div>
